4aee5f7f04a9710481591d33d0199b1f04f0ed09
debeshdeepta <debesh.deepta@soulunileaders.com>
2024-03-19 16:37:05 +0530

    anomaly fixed in app.jsx file
    Route to be child of Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListOfRoles from './components/Admin/UAC/Role/ListOfRoles';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <BrowserRouter>
-        <Route>
+        <Routes>
           <Route path='/' element={<ListOfRoles />} />
-        </Route>
+        </Routes>
       </BrowserRouter>
     </>
   )
