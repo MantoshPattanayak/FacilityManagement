e48be928356c5361749221e8c8b7e5461675c22a
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-03-20 10:04:18 +0530

    logger file added with winston module

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 7100 
 const cors = require('cors');
 var cookieParser = require('cookie-parser')
+const {requestLogger,errorLogger}=require('./middlewares/logger.middlewares')
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -10,20 +11,35 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
+
+// here in the express.urlencoded i.e. extended is equal to true means inside object we can give another object
 app.use(express.urlencoded({
     extended:true,
     limit:"16kb"
 }))
-// here in the express.urlencoded i.e. extended is equal to true means inside object we can give another object
-
-
-app.use(express.static("public"))
 
 //  here to access the public assets like images, folders
+app.use(express.static("public"))
 
-app.use(cookieParser())
 
 //  Here cookie parser is used to do crud operation in the user cookie by the server
+app.use(cookieParser())
+
+// Use the informational logger middleware before all route handlers
+app.use(requestLogger);
+
+
+
+//  put all your route handlers here
+
+
+
+
+
+
+// Use error logger middleware after all route handlers
+app.use(errorLogger);
+
 
 module.exports = {
     app
---
+++ backend/src/middlewares/logger.middlewares.js
@@ -0,0 +1,40 @@
+const winston = require('winston');
+
+
+//  creating separate transports for 'info' and 'error' levels 
+const infoTransport = new winston.transports.File({
+    filename:'info.log', 
+    level:'info'
+})
+
+
+const errorTransport = new winston.transports.File({
+    filename:'error.log',
+    level:'error'
+})
+
+const logger = winston.createLogger({
+    format: winston.format.json(),
+    transports:[
+        new winston.transports.Console(), //log to the console
+        infoTransport, // Log 'info' level messages to info.log
+        errorTransport  // Log 'error' level messages to error.log
+    ]
+})
+
+
+// Middleware function to log incoming requests
+function requestLogger(req, res, next) {
+    logger.log('info', `${req.method} ${req.url}`, { timestamp: Date.now() });
+    next();
+  }
+  
+  // Middleware function to log errors
+  function errorLogger(err, req, res, next) {
+    logger.log('error', err.message, { timestamp: Date.now() });
+    next(err);
+  }
+  module.exports = { 
+    requestLogger,
+     errorLogger
+     };
