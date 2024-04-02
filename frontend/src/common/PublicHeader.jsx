e332e7e41fb0b88ac682087cb2303452b926a22e
debeshdeepta <debesh.deepta@soulunileaders.com>
2024-04-02 15:17:49 +0530

    RolePage(UAC)
    RolePage(UAC)

---
+++ frontend/src/common/PublicHeader.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import "./PublicHeader.css";
+import { FaRegCircleUser } from "react-icons/fa6";
+import APP_LOGO from "../assets/ama-bhoomi-app-logo.png";
+
+const AdminHeader = () => {
+  const [isMenuActive, setMenuActive] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuActive(!isMenuActive);
+    if (activeDropdown !== null) {
+      setActiveDropdown(null);
+    }
+  };
+
+  const collapseSubMenu = () => {
+    if (activeDropdown) {
+      activeDropdown.querySelector(".submenu").removeAttribute("style");
+      activeDropdown.classList.remove("active");
+      setActiveDropdown(null);
+      console.log("collapse Submmanue")
+    }
+  };
+
+  const toggleSubMenu = (e) => {
+    if (e.target.hasAttribute("data-toggle") && window.innerWidth <= 992) {
+      e.preventDefault();
+      const menuDropdown = e.target.parentElement;
+
+      if (menuDropdown === activeDropdown) {
+        collapseSubMenu();
+      } else {
+        collapseSubMenu();
+        menuDropdown.classList.add("active");
+        const subMenu = menuDropdown.querySelector(".submenu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        setActiveDropdown(menuDropdown);
+      }
+    }
+  };
+
+  const resizeWindow = () => {
+    if (window.innerWidth > 992) {
+      setMenuActive(false);
+      collapseSubMenu();
+    }
+  };
+
+  const handleClickOutside = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setMenuActive(false);
+      collapseSubMenu();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeWindow);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("resize", resizeWindow);
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="header" id="header">
+      <section className="wrapper container">
+        <a href="./index.html" className="brand">
+          <img  className="ama_bhoomi_logo"     src={APP_LOGO} alt="App Logo" />
+        </a>
+        <div className="burger" id="burger" onClick={toggleMenu}>
+          <span className="burger-line"></span>
+          <span className="burger-line"></span>
+          <span className="burger-line"></span>
+        </div>
+        {isMenuActive && <div className="overlay" onClick={toggleMenu}></div>}
+        <nav ref={menuRef} className={`navbar ${isMenuActive ? "active" : ""}`} id="navbar" onClick={toggleSubMenu}>
+          <ul className="menu" id="menu">
+            <li className="menu-item menu-dropdown">
+              <span className="menu-link" data-toggle="submenu">English<FontAwesomeIcon icon={faChevronDown}  /></span>
+              <ul className="submenu">
+                <li className="submenu-item"><a href="#" className="submenu-link">Odia</a></li>
+                <li className="submenu-item"><a href="#" className="submenu-link">Hindi</a></li>
+              </ul>
+            </li>
+            <li className="menu-item"><a href="#" className="menu-link">BOOK</a></li>
+            <li className="menu-item"><a href="#" className="menu-link">EVENTS</a></li>
+            
+            <li className="menu-item"><a href="#" className="menu-link">Sign-in</a></li>
+        
+
+
+       
+      
+          
+        
+            {/*  */}
+            
+           
+          
+           
+       <li>
+         
+         
+       </li>
+       
+          </ul>
+          
+        </nav>
+      </section>
+    </header>
+  );
+};
+
+export default AdminHeader;
---
+++ frontend/src/components/Admin/UAC/Role/EditRole.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const EditRole = () => {
+  return (
+    <div className='container-1'>
+
+    <div className="header-role">
+      <div className="rectangle"></div>
+      <div className="roles">
+        <h1><b>Jaydev vatika, Bhubaneswar</b></h1>
+      </div>
+    </div>
+
+    {/* input fields */}
+    <div className="input-fields">
+      <div className="input-field">
+        <label htmlFor="roleName">Role Name:</label>
+        <input type="text" id="roleName" className='search_input_field-2' placeholder='' />
+      </div>
+      <div className="input-field">
+        <label htmlFor="roleCode">Role Code:</label>
+        <input type="text" id="roleCode" className='search_input_field-2' placeholder='' />
+      </div>
+    </div>
+
+    <div className="buttons">
+      {/* cancel button */}
+      <div className="cancel-btn">
+        <button>Cancel</button>
+      </div>
+
+      {/* create button */}
+      <div className="create-btn">
+        <button>Update</button>
+      </div>
+    </div>
+
+  </div>
+  )
+}
+
+export default EditRole
