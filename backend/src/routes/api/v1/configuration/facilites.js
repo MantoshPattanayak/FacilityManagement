74cddb294ff71a858e0bba5c042fd1b17dcf0e9f
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-04-02 17:30:17 +0530

    created park fetch api and park search api

---
+++ backend/src/routes/api/v1/configuration/facilites.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+let api_version = process.env.API_VERSION;
+
+let facilities = require('../../../../controllers/'+api_version+'/configuration/facilities.controllers')
+
+router.get('/displayMapData',facilities.displayMapData)
+router.get('/searchParkFacilities',facilities.searchParkFacilities)
+
+
+module.exports = router
