a4ced3180a3b6c4003d9aa5da4717a39487fc198
Saheli Trushna <saheli.trushna@soulunileaders.com>
2024-04-05 10:26:23 +0530

    added the role master model apis

--- backend/src/controllers/v1/auth/user.controllers.js
+++ backend/src/controllers/v1/auth/user.controllers.js
@@ -1,31 +1,43 @@
-const db = require('../../../models/index')
+const db = require("../../../models/index");
 let statusCode = require("../../../utils/statusCode");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const user = db.publicuser;
 
-const {encrypt} = require('../../../middlewares/encryption.middlewares')
-const {decrypt} = require('../../../middlewares/decryption.middlewares')
+const { encrypt } = require("../../../middlewares/encryption.middlewares");
+const { decrypt } = require("../../../middlewares/decryption.middlewares");
 
-let signUp = async (req,res)=>{
-    const { userName, email, password,roleId,title, firstName,middleName,lastName,phoneNo,altPhoneNo,userImage,remarks} = req.body;
+let signUp = async (req, res) => {
+  const {
+    userName,
+    email,
+    password,
+    roleId,
+    title,
+    firstName,
+    middleName,
+    lastName,
+    phoneNo,
+    altPhoneNo,
+    userImage,
+    remarks,
+  } = req.body;
 
+  const decryptUserName = decrypt(userName);
+  const decryptEmailId = decrypt(email);
+  const decryptPhoneNumber = decrypt(phoneNo);
 
-    const decryptUserName = decrypt(userName);
-    const decryptEmailId = decrypt(email);
-    const decryptPhoneNumber = decrypt(phoneNo);
-    
-    let lastLogin = new Date();
-    let updatedOn =  new Date();
-    let createdOn = new Date();
-    let deletedOn = new Date();
+  let lastLogin = new Date();
+  let updatedOn = new Date();
+  let createdOn = new Date();
+  let deletedOn = new Date();
   // Hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
-  let createdBy= req.user.id;
+  let createdBy = req.user.id;
   let updatedBy = req.user.id;
 
   try {
     // Create a new user record in the database
-   
+
     const uploadDir = process.env.UPLOAD_DIR;
 
     // Ensure that the base64-encoded image data is correctly decoded before writing it to the file. Use the following code to decode the base64 data:
@@ -54,9 +66,8 @@ let signUp = async (req,res)=>{
       userImagePath2 = `/publicUsers/${userId}_driver_image.png`;
     }
 
-
     const newUser = await user.create({
-      roleId:roleId,
+      roleId: roleId,
       title: title,
       firstName: firstName,
       middleName: middleName,
@@ -76,25 +87,20 @@ let signUp = async (req,res)=>{
       updatedOn: new Date(), // Set current timestamp for updatedOn
       // Ensure to set other fields accordingly
       deletedBy: null,
-      deletedOn:null
-  });
-    
-
-
+      deletedOn: null,
+    });
 
     // Return success response
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
   } catch (error) {
     // Handle errors
-    console.error('Error creating user:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
-
-
-
-
 module.exports = {
-  signUp
-}
+  signUp,
+};
