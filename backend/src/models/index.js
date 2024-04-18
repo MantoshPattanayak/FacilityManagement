520694fefd3a50ebfebc51e1ea68982b4662ec08
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-04-18 11:09:51 +0530

    removed the encrypted format in the uac code fixed and create user resource table and gender master table and fixed the bugs in the role resource table

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -48,7 +48,8 @@ sequelize.authenticate()
   db1.statusmaster =require('./statusmaster.models')(sequelize,DataTypes)
   db1.transaction =require('./transactions.models')(sequelize,DataTypes)
   db1.transactiondetails = require('./transactiondetails.models')(sequelize,DataTypes)
-
+  db1.userresource = require('./userresource.models')(sequelize,DataTypes)
+  db1.gendermaster = require('./gendermaster.models')(sequelize,DataTypes)
 
   db1.sequelize.sync({
     force: true
