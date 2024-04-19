4a3e9e51ea15f78b1ceda779af217ef2740452c8
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-04-19 10:41:21 +0530

    added the params in the role route

--- backend/src/routes/api/v1/configuration/role.js
+++ backend/src/routes/api/v1/configuration/role.js
@@ -7,7 +7,7 @@ let role = require("../../../../controllers/" +
   api_version +
   "/configuration/role.controller");
 
-router.get("/roleId", role.roleId);
+router.get("/roleId/:roleId", role.roleId);
 router.put("/updateRole", role.updateRole);
 router.post("/createRole", role.createRole);
 router.post("/viewRole", role.viewRole);
