84f26d430082bf362594c781ed8bf0c0ae883748
ramya-naik <ramya.naik@soulunileaders.com>
2024-04-19 14:03:11 +0530

    modification in encryptData
    modification in encryptData

--- frontend/src/utils/encryptData.js
+++ frontend/src/utils/encryptData.js
@@ -7,13 +7,26 @@ const iv = instance().IV;
 export function encryptData (val) {
     let parsedKey = CryptoJS.enc.Base64.parse(key);
     let parsediv = CryptoJS.enc.Base64.parse(iv);
-    return CryptoJS.AES.encrypt(JSON.stringify(val),parsedKey,{iv:parsediv}).toString();
+    let encryptedData = null;
+    if (typeof val === 'string' && val !== null) {
+        encryptedData = CryptoJS.AES.decrypt(val,parsedKey,{iv:parsediv}).toString(CryptoJS.enc.Utf8);
+    }
+    else {
+        encryptedData = CryptoJS.AES.encrypt(JSON.stringify(val),parsedKey,{iv:parsediv}).toString();
+    }
+    return encryptedData;
 }
 
 export function decryptData (val) {
     let parsedKey = CryptoJS.enc.Base64.parse(key);
     let parsediv = CryptoJS.enc.Base64.parse(iv);
-
-    return JSON.parse(CryptoJS.AES.decrypt(val,parsedKey,{iv:parsediv}).toString(CryptoJS.enc.Utf8));
+    let decryptedData = null;
+    if (typeof val === 'string' && val !== null) {
+        decryptedData = CryptoJS.AES.decrypt(val,parsedKey,{iv:parsediv}).toString(CryptoJS.enc.Utf8);
+    }
+    else {
+        decryptedData = JSON.parse(CryptoJS.AES.decrypt(val,parsedKey,{iv:parsediv}).toString(CryptoJS.enc.Utf8));
+    }
+    return decryptedData;
 }
 
