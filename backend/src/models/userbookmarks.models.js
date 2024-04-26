a8739817ea2f2f12e2c9d001fc2ea8f388569e0e
ramya-naik <ramya.naik@soulunileaders.com>
2024-04-26 17:12:52 +0530

    added db model object for public user bookmarks table
    added db model object for public user bookmarks table

---
+++ backend/src/models/userbookmarks.models.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Bookmarks = sequelize.define('publicUserBookmarks', {
+        bookmarkId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        publicUserId: {
+            type: DataTypes.INTEGER
+        },
+        facilityId: {
+            type: DataTypes.INTEGER
+        },
+        eventId: {
+            type: DataTypes.INTEGER
+        },
+        statusId: {
+            type: DataTypes.INTEGER
+        },
+        remarks: {
+            type: DataTypes.STRING(255)
+        },
+        createdBy: {
+            type: DataTypes.INTEGER
+        },
+        createdOn: {
+            type: DataTypes.DATE, // Define the column as DATE type
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP'), // Set default value to current timestamp
+        },
+        updatedBy: {
+            type: DataTypes.INTEGER
+        },
+        updatedOn: {
+            type: DataTypes.DATE // Define the column as DATE type
+        },
+        deletedBy: {
+            type: DataTypes.INTEGER
+        },
+        deletedOn: {
+            type: DataTypes.DATE // Define the column as DATE type
+        }
+    })
+    return Bookmarks
+}
