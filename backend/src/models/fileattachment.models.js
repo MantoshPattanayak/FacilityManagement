aea62100fc4ab6ed8963e4bca51f5b852175948c
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-04-26 12:46:41 +0530

    created two tables one is file table and another is the file attachement table

---
+++ backend/src/models/fileattachment.models.js
@@ -0,0 +1,44 @@
+
+module.exports = (sequelize,DataTypes)=>{
+    const Fileattachment = sequelize.define('fileattachment',{ 
+        attachmentId:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        entityId:{
+            type:DataTypes.INTEGER
+        },
+        entityType:{
+            type:DataTypes.STRING(180)
+        },
+        fileId:{
+            type:DataTypes.INTEGER
+        },
+        statusId:{
+            type:DataTypes.INTEGER
+        },
+        createdBy:{
+            type:DataTypes.INTEGER
+        },
+        createdDt:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:sequelize.literal("CURRENT_TIMESTAMP")
+        },
+        updatedBy:{
+            type:DataTypes.INTEGER
+
+        },
+        updatedDt:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:sequelize.literal("CURRENT_TIMESTAMP")
+        }
+
+            
+    }
+       
+    )
+    return Fileattachment
+}
