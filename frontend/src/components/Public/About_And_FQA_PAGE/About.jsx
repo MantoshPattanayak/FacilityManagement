1bb0933b5f993979ee399e527e4537b46c97850c
Alok Kumar <alok.kumar@soulunileaders.com>
2024-05-06 19:41:28 +0530

    About_Page
    About_Page

--- frontend/src/components/Public/About_And_FQA_PAGE/About.jsx
+++ frontend/src/components/Public/About_And_FQA_PAGE/About.jsx
@@ -2,6 +2,7 @@
 
 import PublicHeader from "../../../common/PublicHeader";
 import "./About.css"
+import Aboutusimg from "../../../assets/About_us3.png"
 const About=()=>{
  return(
     <div className="About_Mian_conatiner">
@@ -9,7 +10,20 @@ const About=()=>{
      <div className="Child_about_container">
        <h1 className="About"> About Us</h1>
      </div>
-     <h1 className="text-5xl text-center"> Coming Soon</h1>
+      <div className="About_div">
+         <h1 className="About_heading"> About</h1>
+         <div className="About_us_flex_conatiner">
+            <div className="About_us_text">
+             <h1 className="About_us_text_details" >Easy Parking Solutions: With Park, finding parking spots becomes a breeze. Our intuitive app allows  you to search for available <br></br> parking spaces in real-time, helping you save time and reduce stress.
+Secure . </h1>
+             
+            </div>
+            <div className="About_us_img">
+              <img className="About_img" src={Aboutusimg}></img>
+            </div>
+         </div>
+      </div>
+      
     </div>
     )
 }
