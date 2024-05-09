fcbbd12f829852dab8c993f7c36aafc71aa91061
debeshdeepta <debesh.deepta@soulunileaders.com>
2024-05-09 18:14:05 +0530

    Evet list , Book Event and Details pages of Event (Public)
    Evet list , Book Event and Details pages of Event (Public)

--- frontend/src/components/Public/Events/BookEvent.jsx
+++ frontend/src/components/Public/Events/BookEvent.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
-
-export default function BookEvent() {
+import '../Events/BookEvent.css'
+const BookEvent = () => {
   return (
-    <div>BookEvent</div>
+    <div className='event-container'>
+      <form action="" className="book-event">
+        <div className="heading">
+          <p>International Odishi dance festival</p>
+        </div>
+        <div className="total-members">
+          <label htmlFor="">Total Members:</label>
+          <div className="input-bookevent">
+            <input type="text" />
+          </div>
+        </div>
+
+        <div className="total-members">
+          <label htmlFor="">Total Members:</label>
+          <div className="input-bookevent">
+            <input type="date" />
+          </div>
+        </div>
+
+        <div className="total-members">
+          <label htmlFor="">Total Members:</label>
+          <div className="input-bookevent">
+            <input type="time" />
+          </div>
+        </div>
+
+        <div className="total-members">
+          <label htmlFor="">Total Members:</label>
+          <div className="input-bookevent">
+            <input type="text" />
+          </div>
+        </div>
+        <button className="add-to-cart-btn">
+          Add to Cart
+        </button>
+      </form>
+      
+    </div>
   )
 }
+
+export default BookEvent
--- frontend/src/components/Public/Events/EventList.jsx
+++ frontend/src/components/Public/Events/EventList.jsx
@@ -1,7 +1,393 @@
 import React from 'react'
+import '../Events/EventList.css'
+import Cardimg from "../../../assets/Card_img.png";
+
+
+// Font Awesome icon --------------------------------
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+// Axios for Call the API --------------------------------
+import axiosHttpClient from "../../../utils/axios";
+// Common footer---------------------------- ----------------
+import CommonFooter from "../../../common/CommonFooter";
+// Import Logo here ------------------------------ ------------------------------
+import Park_Logo from "../../../assets/Ground_logo.png";
+import MultiPark from "../../../assets/ama_bhoomi_multipurpose_grounds_logo-removebg-preview.png";
+import Event_img from "../../../assets/ama_boomi_park_logo-removebg-preview.png";
+// Data Not available Icon ---------------------------------------------------------------
+import No_Data_icon from "../../../assets/No_Data_icon.png";
+import { useEffect, useState } from "react";
+// Import here to encrptData ------------------------------------------
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { decryptData, encryptData } from "../../../utils/encryptData";
+import PublicHeader from "../../../common/PublicHeader";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+// impport shimmerUi------------------------------------------
+import ShimmerUI from "./ShimmerUI";
+
 
 export default function EventList() {
+
+  const defaultLocation = {
+    latitude: 20.3010259,
+    longitude: 85.7380521
+  }
+  // Use state ------------------------------------------------------------------
+  const [DisPlayParkData, setDisPlayParkData] = useState([]);
+  // const simmerUi Loader------------------------------------------------
+  const [IsLoding, setIsLoding] = useState(false);
+  // useSate for search -----------------------------------------------------------
+  const [givenReq, setGivenReq] = useState("");
+  // State variable to track selected layout type ------------------------------
+  const [layoutType, setLayoutType] = useState('grid');
+
+  // here set the latitude and longitude --------------------------------------------
+  const [userLocation, setUserLocation] = useState();
+  // fetch facility type id --------------------------------------------------------
+  let location = useLocation();
+  // for Faciltiy ----------------------------------------------------------------
+  const [facilityTypeId, setFacilityTypeId] = useState();
+  const [selectedTab, setSelectedTab] = useState('');
+  const tabList = ['Nearby', 'Popular', 'Free', 'Paid']
+
+  // Use Navigate for Navigate the page ----------------------------------------------
+  let navigate = useNavigate();
+
+  // Function to handle layout change
+  const handleLayoutChange = (e) => {
+    setLayoutType(e.target.value);
+  };
+
+  //Here (Post the data)----------------------------------------------------------------
+  async function GetParkDetails() {
+    try {
+      setIsLoding(true)
+      let res = await axiosHttpClient("View_Park_Data", "post", {
+        givenReq: givenReq,
+        facilityTypeId: facilityTypeId,
+      });
+      console.log("here Response of Park", res);
+      setDisPlayParkData(res.data.data);
+      setIsLoding(false);
+    } catch (err) {
+      console.log("here Error of Park", err);
+      setIsLoding(false);
+    }
+  }
+
+  // Function to handle selectedTab button click  ------------------------------
+  const handleTabClick = (e, tab) => {
+    if (tab != selectedTab)
+      setSelectedTab(tab);
+    else
+      setSelectedTab();
+    console.log('selectedTab', selectedTab);
+  }
+
+  // here set the userLocation -------------------------------------------------------
+  function setUserGeoLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+          sessionStorage.setItem('location', JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          }));
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+          // setUserLocation(defaultLocation);
+          // sessionStorage.setItem('location', JSON.stringify(location));
+          // Handle error, e.g., display a message to the user
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser');
+      toast.error('Location permission not granted.');
+    }
+    return;
+  }
+
+  // filter the data according to filter options -----------------------------------------------------
+  async function getNearbyFacilities() {
+    let bodyParams = {};
+    switch (selectedTab) {    // ['Nearby', 'Popular', 'Free', 'Paid']
+      case 'Nearby':
+        setUserGeoLocation();
+        bodyParams = {
+          latitude: userLocation.latitude,
+          longitude: userLocation.longitude,
+          facilityTypeId: facilityTypeId,
+          range: ''
+        }
+        break;
+      case 'Popular': bodyParams = { popular: 1, facilityTypeId: facilityTypeId }; break;
+      case 'Free': bodyParams = { free: 1, facilityTypeId: facilityTypeId }; break;
+      case 'Paid': bodyParams = { paid: 1, facilityTypeId: facilityTypeId }; break;
+      default: break;
+    }
+
+    try {
+      setIsLoding(true)
+      let res = await axiosHttpClient('VIEW_NEARBY_PARKS_API', 'post', bodyParams);
+      console.log(res.data.message, res.data.data);
+      setDisPlayParkData(res.data.data);
+      setIsLoding(false)
+    }
+    catch (error) {
+      console.error(error);
+      toast.error('Location permission not granted.')
+      setIsLoding(false)
+    }
+  }
+
+  // Function to handle setting facility type ID and updating search input value ---------------------------
+  const handleParkLogoClick = (e, typeid) => {
+    setFacilityTypeId(typeid); // Set facility ex typeid-1,typeid-2,typeid-3
+    console.log("here type id", { typeid });
+  };
+
+  // here Function to encryptDataid (Pass the Id)----------------------------------------------
+  function encryptDataId(id) {
+    let res = encryptData(id);
+    return res;
+  }
+
+  // on page load
+  useEffect(() => {
+    let facilityIdTypeFetch = decryptData(new URLSearchParams(location.search).get("facilityTypeId"));
+    let givenReqFetch = new URLSearchParams(location.search).get("givenReq");
+    try {
+      let userLocation = JSON.parse(sessionStorage?.getItem('location'));
+      setUserLocation(userLocation);
+    }
+    catch(error) {
+      console.error('json parse error');
+      sessionStorage.setItem('location', JSON.stringify({}));
+    }
+    // on page load, if we have facilityTypeId from homepage, 
+    // then set facilityType or else set default facilityType to 1 (Parks)
+    if (facilityIdTypeFetch)
+      setFacilityTypeId(facilityIdTypeFetch);
+    else
+      setFacilityTypeId(1);
+
+    if(givenReqFetch)
+      setGivenReq(givenReqFetch);
+    else
+      setGivenReq('');
+  }, [])
+
+  // refresh page on searching, change in facility type or selecting filter options
+  useEffect(() => {
+    if (selectedTab) {  //fetch facility details based on filter options
+      getNearbyFacilities()
+    }
+    else { // fetch facility details based on selected facility type or searching value
+      GetParkDetails();
+    }
+  }, [givenReq, facilityTypeId, selectedTab]);
+
+
+
+
   return (
-    <div>EventList</div>
+    <div className="event-main__body__park">
+    {/* here Header -----------------------------------------------------*/}
+    <PublicHeader />
+    {/* Here Below of header set image ---------------------------------------------------- */}
+    <div
+      className={`${(facilityTypeId == 1)
+          ? "event-park-body-1"
+          : (facilityTypeId == 2)
+            ? "event-park-body-2"
+            : (facilityTypeId == 3)
+              ? "event-park-body-3"
+              : ""
+        }`}
+    >
+      <h1 className="event-name_park_img">
+        {(facilityTypeId == 1) && "Parks"}
+        {(facilityTypeId == 2) && "Sports"}
+        {(facilityTypeId == 3) && "Multi-purpose Grounds"}
+      </h1>
+    </div>
+    {/* here Search  Bar  -------------------------------------------------- */}
+    <div className="event-Search_container">
+      <span className="event-Input_text_conatiner">
+        <div className="event-search-container1">
+          <input
+            type="text"
+            className="event-Search_input"
+            placeholder="Search by Name, Location........"
+            name="givenReq"
+            id="givenReq"
+            value={givenReq}
+            onChange={(e) => setGivenReq(e.target.value)}
+          />
+          <div className="event-search-icon">
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+        </div>
+      </span>
+      {/* Here Button Container (Pass the id 1,2,3) ------------------------------*/}
+      <span className="event-Button_Container">
+        {/* Park */}
+        <button
+          onClick={(e) => handleParkLogoClick(e, 1)}
+          className="event-image-button"
+        >
+          <img className="h-14" src={Event_img} alt="Event" />
+          <span className="event-button-text">Park</span>
+        </button>
+
+        {/* Sports */}
+        <button
+          onClick={(e) => handleParkLogoClick(e, 2)}
+          className="event-image-button"
+        >
+          <img className="h-14" src={Park_Logo} alt="Sports" />
+          <span className="event-button-text">Sports</span>
+        </button>
+
+        {/* Multipark */}
+        <button
+          onClick={(e) => handleParkLogoClick(e, 3)}
+          className="event-image-button"
+        >
+          <img className="h-14" src={MultiPark} alt="Multipark" />
+          <span className="event-button-text">Multipark</span>
+        </button>
+      </span>
+    </div>
+    {/* Filter According to free, paid, NearBy, ------------------ */}
+    <div className="event-Filter_grid">
+      <div className="event-filter_button">
+        {
+          tabList?.length > 0 && tabList?.map((tab) => {
+            return (
+              <button className={`button-59 ${selectedTab == tab ? 'bg-[#19ba62] text-white' : ''}`} role="button" onClick={(e) => { handleTabClick(e, tab) }}>
+                {tab}
+              </button>
+            )
+          })
+        }
+      </div>
+      <div class="event-gride_page">
+        <select name="layout" id="layout" value={layoutType} onChange={handleLayoutChange}>
+          <option value="grid">Grid</option>
+          <option value="list">List</option>
+
+        </select>
+      </div>
+
+
+    </div>
+    {/* Heere Name of Park, sports dyamics ---------------------------------------- */}
+    <span className="event-text_name_park">
+      <h1 className="event-name_park">
+        {(facilityTypeId == 1) && "Parks"}
+        {(facilityTypeId == 2) && "Sports"}
+        {(facilityTypeId == 3) && "Multi-purpose Grounds"}
+      </h1>
+    </span>
+
+    {/* Card Container Here -------------------------------------------- */}
+
+
+    <div className="event-card-container">
+      {IsLoding ? (
+        // Show shimmer loader while data is being fetched
+        <ShimmerUI />
+      ) : DisPlayParkData?.length > 0 ? (
+        // Conditional rendering based on layout type
+        layoutType === 'grid' ? (
+          DisPlayParkData?.map((item, index) => (
+            <Link
+              key={index}
+              to={{
+                pathname: "/Sub_Park_Details",
+                search: `?facilityId=${encryptDataId(item.facilityId)}&action=view`,
+              }}
+            >
+              <div
+                className={`${item.facilityTypeId === 1
+                    ? "event-park-card-1"
+                    : item.facilityTypeId === 2
+                      ? "event-park-card-2"
+                      : "event-park-card-3"
+                  }`}
+              >
+                <img className="event-Card_img" src={Cardimg} alt="Park" />
+                <div className="event-card_text">
+                  <span className="event-Name_location">
+                    <h2 className="event-park_name">{item.facilityname}</h2>
+                    <h3 className="event-park_location">{item.address}</h3>
+                  </span>
+                  <span className="event-Avil_Dis">
+                    <button
+                      className={`event-Avilable ${item.status == "open" ? "text-green-500" : "text-red-500"
+                        }`}
+                    >
+                      {item.status?.charAt(0).toUpperCase() + item.status?.slice(1)}
+                    </button>
+                    <h3 className="event-distance">{Number(item.distance?.toFixed(2)) || 10} km(s)</h3>
+                  </span>
+                </div>
+              </div>
+            </Link>
+          ))
+        ) : (
+          // here Table data (Data in list)----------------------------------------------------------------
+          <div className="event-table_Container1">
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Name of the Park</th>
+                  <th scope="col">Location</th>
+                  <th className="event-left">Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                {DisPlayParkData?.length > 0 && DisPlayParkData.map((table_item, table_index) => (
+                  <tr key={table_index}>
+                    <td data-label="Name">{table_item.facilityname}</td>
+                    <td data-label="Location">{table_item.address}</td>
+                    <td className="left text-green-700 text-xl font-medium"> {/* Wrap Details within the <td> */}
+                      <Link
+                        key={table_index}
+                        to={{
+                          pathname: "/Sub_Park_Details",
+                          search: `?facilityId=${encryptDataId(table_item.facilityId)}`,
+                        }}
+                      >
+                        Details
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+        )
+      ) : (
+        // Show message if no data is available
+        <div className="event-no-data-message">
+          <img src={No_Data_icon} alt="No Data Found" />
+        </div>
+      )}
+    </div>
+
+
+
+    <CommonFooter />
+    <ToastContainer />
+  </div>
   )
 }
