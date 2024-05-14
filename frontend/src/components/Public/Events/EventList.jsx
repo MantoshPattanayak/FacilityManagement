6d1aee672f4cd55be1712a7edb955648d52475bd
ramya-naik <ramya.naik@soulunileaders.com>
2024-05-14 15:10:51 +0530

    list of events page css responsiveness
    list of events page css responsiveness

--- frontend/src/components/Public/Events/EventList.jsx
+++ frontend/src/components/Public/Events/EventList.jsx
@@ -48,9 +48,6 @@ export default function EventList() {
   const [selectedTab, setSelectedTab] = useState('');
   const tabList = ['Nearby', 'Popular', 'Free', 'Paid']
 
-  // Use Navigate for Navigate the page ----------------------------------------------
-  let navigate = useNavigate();
-
   // Function to handle layout change
   const handleLayoutChange = (e) => {
     setLayoutType(e.target.value);
@@ -164,154 +161,146 @@ export default function EventList() {
 
   return (
     <div className="event-main__body__park">
-    {/* here Header -----------------------------------------------------*/}
-    <PublicHeader />
-    {/* Here Below of header set image ---------------------------------------------------- */}
-    <div
-      className="event-body"
-    >
-      <h1 className="event-name_park_img">
-        Events
-      </h1>
-    </div>
-    {/* here Search  Bar  -------------------------------------------------- */}
-    <div className="event-Search_container">
-      <span className="event-Input_text_conatiner">
-        <div className="event-search-container1">
-          <input
-            type="text"
-            className="event-Search_input"
-            placeholder="Search by Name, Location........"
-            name="givenReq"
-            id="givenReq"
-            value={givenReq}
-            onChange={(e) => setGivenReq(e.target.value)}
-          />
-          <div className="event-search-icon">
-            <FontAwesomeIcon icon={faSearch} />
+      <PublicHeader />
+      {/* here Header -----------------------------------------------------*/}
+      
+      {/* Here Below of header set image ---------------------------------------------------- */}
+      <div
+        className="event-body"
+      >
+        <h1 className="event-name_park_img">
+          Events
+        </h1>
+      </div>
+      {/* here Search  Bar  -------------------------------------------------- */}
+      <div className="event-Search_container">
+        <span className="event-Input_text_conatiner">
+          <div className="event-search-container1">
+            <input
+              type="text"
+              className="event-Search_input"
+              placeholder="Search by Name, Location........"
+              name="givenReq"
+              id="givenReq"
+              value={givenReq}
+              onChange={(e) => setGivenReq(e.target.value)}
+            />
+            <div className="event-search-icon">
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
           </div>
+        </span>
+      </div>
+      {/* Filter According to free, paid, NearBy, ------------------ */}
+      <div className="event-Filter_grid">
+        <div className="event-filter_button">
+          {
+            tabList?.length > 0 && tabList?.map((tab) => {
+              return (
+                <button className={`event-button-59 ${selectedTab == tab ? 'bg-[#19ba62] text-white' : ''}`} role="button" onClick={(e) => { handleTabClick(e, tab) }}>
+                  {tab}
+                </button>
+              )
+            })
+          }
         </div>
-      </span>
-    </div>
-    {/* Filter According to free, paid, NearBy, ------------------ */}
-    <div className="event-Filter_grid">
-      <div className="event-filter_button">
-        {
-          tabList?.length > 0 && tabList?.map((tab) => {
-            return (
-              <button className={`button-59 ${selectedTab == tab ? 'bg-[#19ba62] text-white' : ''}`} role="button" onClick={(e) => { handleTabClick(e, tab) }}>
-                {tab}
-              </button>
-            )
-          })
-        }
+        <div class="event-gride_page ml-1">
+          <select name="layout" id="layout" value={layoutType} onChange={handleLayoutChange}>
+            <option value="grid">Grid</option>
+            <option value="list">List</option>
+          </select>
+        </div>
       </div>
-      <div class="event-gride_page">
-        <select name="layout" id="layout" value={layoutType} onChange={handleLayoutChange}>
-          <option value="grid">Grid</option>
-          <option value="list">List</option>
-        </select>
-      </div>
-    </div>
-
-    {/* Heere Name of Park, sports dyamics ---------------------------------------- */}
-    {/* <span className="event-text_name_park">
-      <h1 className="event-name_park">
-        Events
-      </h1>
-    </span> */}
-
-    {/* Card Container Here -------------------------------------------- */}
 
 
-    <div className="event-card-container">
-      {IsLoding ? (
-        // Show shimmer loader while data is being fetched
-        <ShimmerUI />
-      ) : eventListData?.length > 0 ? (
-        // Conditional rendering based on layout type
-        layoutType === 'grid' ? (
-          eventListData?.map((item, index) => (
-            <Link
-              key={index}
-              to={{
-                pathname: "/events-details",
-                search: `?eventId=${encryptDataId(item.eventId)}`,
-              }}
-            >
-              <div
-                className="event-park-card-1"
-                title={item.eventName}
+      {/* Card Container Here -------------------------------------------- */}
+
+
+      <div className="event-card-container">
+        {IsLoding ? (
+          // Show shimmer loader while data is being fetched
+          <ShimmerUI />
+        ) : eventListData?.length > 0 ? (
+          // Conditional rendering based on layout type
+          layoutType === 'grid' ? (
+            eventListData?.map((item, index) => (
+              <Link
+                key={index}
+                to={{
+                  pathname: "/events-details",
+                  search: `?eventId=${encryptDataId(item.eventId)}`,
+                }}
               >
-                <img className="event-Card_img" src={EventCardPic} alt="Park" />
-                <div className="event-card_text">
-                  <span className="event-Name_location">
-                    <h2 className="event-park_name">{truncateName(item.eventName, 25)}</h2>
-                    <h3 className="event-park_location"><FontAwesomeIcon icon={faLocationDot} /> &nbsp;{truncateName(item.locationName, 25)}</h3>
-                    <div className='flex gap-x-2 justify-between'>
-                      <h4 className='event-park_location'><FontAwesomeIcon icon={faCalendarDays} /> &nbsp;{formatDate(item.eventDate)}</h4>
-                      <h4 className='event-time-details'><FontAwesomeIcon icon={faClock} /> &nbsp;{formatTime(item.eventStartTime)} - {formatTime(item.eventEndTime)}</h4>
-                    </div>
-                  </span>
-                  <span className="event-Avil_Dis">
-                    <button
-                      className={`event-Avilable ${item.status == "ACTIVE" ? "text-green-500" : "text-red-500"
-                        }`}
-                    >
-                      {item.status?.charAt(0).toUpperCase() + item.status?.slice(1)}
-                    </button>
-                    <h3 className="event-distance">{Number(item.distance?.toFixed(2)) || 10} km(s)</h3>
-                  </span>
-                </div>
-              </div>
-            </Link>
-          ))
-        ) : (
-          // here Table data (Data in list)----------------------------------------------------------------
-          <div className="event-table_Container1">
-            <table>
-              <thead>
-                <tr>
-                  <th scope="col">Name of the Event</th>
-                  <th scope="col">Location</th>
-                  <th className="event-left">Event Details</th>
-                </tr>
-              </thead>
-              <tbody>
-                {eventListData?.length > 0 && eventListData.map((table_item, table_index) => (
-                  <tr key={table_index}>
-                    <td data-label="Name">{truncateName(table_item.eventName, 40)}</td>
-                    <td data-label="Location">{truncateName(table_item.locationName, 40)}</td>
-                    <td className="left text-green-700 text-xl font-medium"> {/* Wrap Details within the <td> */}
-                      <Link
-                        key={table_index}
-                        to={{
-                          pathname: "/event-details",
-                          search: `?eventId  =${encryptDataId(table_item.eventId)}`,
-                        }}
+                <div
+                  className="event-park-card-1"
+                  title={item.eventName}
+                >
+                  <img className="event-Card_img" src={EventCardPic} alt="Park" />
+                  <div className="event-card_text">
+                    <span className="event-Name_location">
+                      <h2 className="event-park_name">{truncateName(item.eventName, 25)}</h2>
+                      <h3 className="event-park_location"><FontAwesomeIcon icon={faLocationDot} /> &nbsp;{truncateName(item.locationName, 25)}</h3>
+                      <div className='flex gap-x-2 justify-between'>
+                        <h4 className='event-park_location'><FontAwesomeIcon icon={faCalendarDays} /> &nbsp;{formatDate(item.eventDate)}</h4>
+                        <h4 className='event-time-details'><FontAwesomeIcon icon={faClock} /> &nbsp;{formatTime(item.eventStartTime)} - {formatTime(item.eventEndTime)}</h4>
+                      </div>
+                    </span>
+                    <span className="event-Avil_Dis">
+                      <button
+                        className={`event-Avilable ${item.status == "ACTIVE" ? "text-green-500" : "text-red-500"
+                          }`}
                       >
-                        Details
-                      </Link>
-                    </td>
+                        {item.status?.charAt(0).toUpperCase() + item.status?.slice(1)}
+                      </button>
+                      <h3 className="event-distance">{Number(item.distance?.toFixed(2)) || 10} km(s)</h3>
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            // here Table data (Data in list)----------------------------------------------------------------
+            <div className="event-table_Container1">
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Name of the Event</th>
+                    <th scope="col">Location</th>
+                    <th className="event-left">Event Details</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {eventListData?.length > 0 && eventListData.map((table_item, table_index) => (
+                    <tr key={table_index}>
+                      <td data-label="Name">{truncateName(table_item.eventName, 40)}</td>
+                      <td data-label="Location">{truncateName(table_item.locationName, 40)}</td>
+                      <td className="left text-green-700 text-xl font-medium"> {/* Wrap Details within the <td> */}
+                        <Link
+                          key={table_index}
+                          to={{
+                            pathname: "/event-details",
+                            search: `?eventId  =${encryptDataId(table_item.eventId)}`,
+                          }}
+                        >
+                          Details
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+          )
+        ) : (
+          // Show message if no data is available
+          <div className="event-no-data-message">
+            <img src={No_Data_icon} alt="No Data Found" />
           </div>
-
-        )
-      ) : (
-        // Show message if no data is available
-        <div className="event-no-data-message">
-          <img src={No_Data_icon} alt="No Data Found" />
-        </div>
-      )}
+        )}
+      </div>
+      <ToastContainer />
+      <CommonFooter />
     </div>
-
-
-
-    <CommonFooter />
-    <ToastContainer />
-  </div>
   )
 }
