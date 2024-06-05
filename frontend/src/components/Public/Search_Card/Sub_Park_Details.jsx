72833e8e14b7f4ae5c73db4f9831a6b0da071c4e
debeshdeepta <debesh.deepta@soulunileaders.com>
2024-06-05 10:28:22 +0530

    Add Operating Days in Sub_Park_Details
    Add Days in Sub_Park_Details

--- frontend/src/components/Public/Search_Card/Sub_Park_Details.jsx
+++ frontend/src/components/Public/Search_Card/Sub_Park_Details.jsx
@@ -1,29 +1,29 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import "./Sub_Park_Details.css";
-// Here Import Admin Header AND fOOTER ------------------------------------
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark as faBookmarkSolid } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as faBookmarkRegular } from "@fortawesome/free-regular-svg-icons";
+
 import CommonFooter from "../../../common/CommonFooter";
 import AdminHeader from "../../../common/AdminHeader";
 
-// Location icon and image all types of image---------------------------------------------
 import Location_icon from "../../../assets/Location_goggle_icon-removebg-preview.png";
 import Park_img from "../../../assets/Park_details.jpg";
 import amabhoomi from '../../../assets/ama_bhoomi_bgi.jpg';
-import sport_image from "../../../assets/sport_details_image.jpg"
+import sport_image from "../../../assets/sport_details_image.jpg";
 
 import Yoga_img from "../../../assets/Yoga_img.png";
 import Google_map from "../../../assets/Google_map.jpg";
 import correct_icon from "../../../assets/Correct_icon.png";
 import Phone_icon from "../../../assets/Phone_icon.png";
-// Import Aixos method---------------------------------------------
+
 import axiosHttpClient from "../../../utils/axios";
-// Import Navigate and Crypto -----------------------------------
 import { useLocation, useNavigate } from "react-router-dom";
 import { decryptData } from "../../../utils/encryptData";
-// Import here to encrptData ------------------------------------------
 
 import { Link } from "react-router-dom";
 import { encryptData } from "../../../utils/encryptData";
-// Google MAP --------------------------------
+
 import {
   GoogleMap,
   LoadScript,
@@ -32,25 +32,21 @@ import {
 } from "@react-google-maps/api";
 import PublicHeader from "../../../common/PublicHeader";
 import Book_Now from "../BookParks/Book_Now";
-// Here Funcation of Sub_park_details------------------------------------------
+
 const Sub_Park_Details = () => {
-  // UseSate for get data -------------------------------------
   const [ServiceData, setServiceData] = useState([]);
   const [amenitiesData, setAmenitiesData] = useState([]);
   const [EventAvailable, setEventAvailable] = useState([]);
   const [FacilitiesData, setFacilitiesData] = useState([]);
-  // here Check Login Status------------------------------------
   const [LoginStatus, setLoginStatus] = useState([0]);
-
-  //here Location / crypto and navigate the page---------------
   const location = useLocation();
   const facilityId = decryptData(
     new URLSearchParams(location.search).get("facilityId")
   );
-  const action = new URLSearchParams(location.search).get("action");
   const navigate = useNavigate();
   const isUserLoggedIn = sessionStorage.getItem("isUserLoggedIn") || 0;
   const [toRoute, setToRoute] = useState();
+  const [operatingDays, setOperatingDays] = useState('');
 
   //Here is the popup state
 
@@ -60,15 +56,25 @@ const Sub_Park_Details = () => {
     setShowPopup(!showPopup);
   };
 
-  // Here Map Api keys ------------------------------------------------------------
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  useEffect(() => {
+    const savedBookmarkStatus = localStorage.getItem(`bookmark_${facilityId}`);
+    if (savedBookmarkStatus !== null) {
+      setIsBookmarked(JSON.parse(savedBookmarkStatus));
+    }
+  }, [facilityId]);
+
+  const handleBookmarkClick = () => {
+    const newBookmarkStatus = !isBookmarked;
+    setIsBookmarked(newBookmarkStatus);
+    localStorage.setItem(`bookmark_${facilityId}`, JSON.stringify(newBookmarkStatus));
+  };
 
   const apiKey = "AIzaSyBYFMsMIXQ8SCVPzf7NucdVR1cF1DZTcao";
   const defaultCenter = { lat: 20.2961, lng: 85.8245 };
-  let randomKey = Math.random();
 
-  // Here Get the data of Sub_park_details------------------------------------------
   async function getSub_park_details() {
-    // console.log('facilityId', facilityId);
     try {
       let res = await axiosHttpClient(
         "View_By_ParkId",
@@ -77,79 +83,66 @@ const Sub_Park_Details = () => {
         facilityId
       );
 
-      console.log("here Response", res);
       setServiceData(res.data.serviceData);
       setAmenitiesData(res.data.amenitiesData);
       setEventAvailable(res.data.eventDetails);
       setFacilitiesData(res.data.facilitiesData);
+      setOperatingDaysFromRes(res);
+
+      function setOperatingDaysFromRes(res) {
+        let operatingDaysFromRes = [];
+        if (res.data.facilitiesData[0].sun == 1) {
+          operatingDaysFromRes.push('Sun');
+        }
+        if (res.data.facilitiesData[0].mon == 1) {
+          operatingDaysFromRes.push('Mon');
+        }
+        if (res.data.facilitiesData[0].tue == 1) {
+          operatingDaysFromRes.push('Tue');
+        }
+        if (res.data.facilitiesData[0].wed == 1) {
+          operatingDaysFromRes.push('Wed');
+        }
+        if (res.data.facilitiesData[0].thu == 1) {
+          operatingDaysFromRes.push('Thu');
+        }
+        if (res.data.facilitiesData[0].fri == 1) {
+          operatingDaysFromRes.push('Fri');
+        }
+        if (res.data.facilitiesData[0].sat == 1) {
+          operatingDaysFromRes.push('Sat');
+        }
+        setOperatingDays(operatingDaysFromRes);
+      }
     } catch (err) {
-      console.log("here Error", err);
+      console.log("Error fetching park details", err);
     }
   }
-  // For Find Out the Status of Login Page -----------------------------------
-  // For Find Out the Status of Login Page -------------------------------
 
-  useEffect(() => { }, [isUserLoggedIn]);
-
-  // UseEffect for Update the data---------------------------------------------
   useEffect(() => {
     getSub_park_details();
   }, []);
-  // here Funcation to encrotDataid (Pass the Id)----------------------------------------------
+
   function encryptDataId(id) {
-    let res = encryptData(id);
-    return res;
+    return encryptData(id);
   }
 
-  //Image swap conatiner ------------------------------------------------
-  // const containerRef = useRef(null);
-  // const [currentIndex, setCurrentIndex] = useState(0);
-  // useEffect(() => {
-  // const container = containerRef.current;
-  // const interval = setInterval(() => {
-  // const newIndex = (currentIndex + 1) % 10;
-  // setCurrentIndex(newIndex);
-  // container.scrollLeft = newIndex * container.offsetWidth;
-  // }, 4000);
-
-  // return () => clearInterval(interval);
-  // }, [currentIndex]);
-
-
   function formatTime(time24) {
-    //format 24 hour time as 12 hour time
     if (!time24) return;
-    // Parse the input time string
     const [hours, minutes] = time24.split(":").map(Number);
-
-    // Determine AM or PM
     const period = hours >= 12 ? "PM" : "AM";
-
-    // Convert hours to 12-hour format
-    const hours12 = hours % 12 || 12; // 0 should be 12 in 12-hour format
-
-    // Format the time string
-    const time12 = `${hours12}:${String(minutes).padStart(2, "0")} ${period}`;
-
-    return time12;
+    const hours12 = hours % 12 || 12;
+    return `${hours12}:${String(minutes).padStart(2, "0")} ${period}`;
   }
 
   function formatDate(date) {
-    //format input date as DD-MM-YYYY
     if (!date) return;
-
     const [year, month, day] = date.split("-");
-
     return `${day}-${month}-${year}`;
   }
 
-
-  // carousel logic.........................................
-
   const images = [Park_img, amabhoomi, Park_img, amabhoomi, Park_img];
-
   const [currentIndex1, setCurrentIndex1] = useState(0);
-
 
   const handlePrev = () => {
     setCurrentIndex1(currentIndex1 === 0 ? images.length - 1 : currentIndex1 - 1);
@@ -159,14 +152,10 @@ const Sub_Park_Details = () => {
     setCurrentIndex1(currentIndex1 === images.length - 1 ? 0 : currentIndex1 + 1);
   };
 
-
-  // Here Return Function ------------------------------------------------------------
   return (
     <div className="Sub_Manu_Conatiner">
-      {/* here Header -----------------------------------------------------*/}
       <PublicHeader />
 
-      {/* Here Heading Image (Below of header) and set the class Name according to Id */}
       <div className={FacilitiesData?.length > 0 && FacilitiesData[0]?.facilityTypeId === 1 ? "Header_Img" : FacilitiesData[0]?.facilityTypeId === 2 ? "playground_header_image" : FacilitiesData[0]?.facilityTypeId === 3 ? "MulitGroud" : ""}>
         <h1 className="text-park">
           {FacilitiesData?.length > 0 && FacilitiesData[0]?.facilityName}
@@ -178,24 +167,8 @@ const Sub_Park_Details = () => {
           </h1>
         </span>
       </div>
-      {/*---------------- Jsx for Map and Image ------------------- */}
+
       <div className="map_img_main_conatiner">
-        {/* <div className="Image_conatiner" ref={containerRef}>
-<button className="carousel-button left" onClick={prevImage}>
-&lt;
-</button>
-{images.map((images, index) => (
-<img
-key={index}
-className="Park_image"
-src={images}
-alt={`Images ${index}`}
-/>
-))}
-<button className="carousel-button right" onClick={nextImage}>
-&gt;
-</button>
-</div> */}
         <div className="carousel-container1">
           <div className="carousel1">
             <img src={images[currentIndex1]} alt={`Slide ${currentIndex1 + 1}`} />
@@ -208,24 +181,34 @@ alt={`Images ${index}`}
           </button>
         </div>
 
-
         <div className="Map_container">
-          <span className="time_status">
-            <h1 className="time_text">
-              {" "}
-              Timing : {formatTime(
-                FacilitiesData[0]?.operatingHoursFrom
-              )} - {formatTime(FacilitiesData[0]?.operatingHoursTo)}
-            </h1>
-            <button
-              className={`Open_Button ${FacilitiesData.length > 0 && FacilitiesData[0].status === "open"
-                ? "open"
-                : "closed"
-                }`}
-            >
-              {FacilitiesData?.length > 0 &&
-                FacilitiesData[0]?.status.toUpperCase()}
-            </button>
+          <span className="time_status flex flex-col">
+            <div className="flex">
+              <h1 className="time_text">
+                Timing : {formatTime(
+                  FacilitiesData[0]?.operatingHoursFrom
+                )} - {formatTime(FacilitiesData[0]?.operatingHoursTo)}
+              </h1>
+              <button
+                className={`Open_Button ${FacilitiesData.length > 0 && FacilitiesData[0].status === "open"
+                  ? "open"
+                  : "closed"
+                  }`}
+              >
+                {FacilitiesData?.length > 0 &&
+                  FacilitiesData[0]?.status.toUpperCase()}
+              </button>
+
+              <div className={`bookmark ${isBookmarked ? 'bookmarked' : ''}`} onClick={handleBookmarkClick}>
+                <FontAwesomeIcon icon={isBookmarked ? faBookmarkSolid : faBookmarkRegular} />
+              </div>
+            </div>
+
+            <div>
+              <h1 className="date_text text-[12px]">
+                Day:{operatingDays.toString()}
+              </h1>
+            </div>
           </span>
 
           <span className="Button_ticket_container">
