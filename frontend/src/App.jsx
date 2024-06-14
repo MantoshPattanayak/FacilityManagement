7c9d6fac0718c7ddd8fcca4aa611a3cc905f0394
ramya-naik <ramya.naik@soulunileaders.com>
2024-06-14 16:14:17 +0530

    resource api and user admin api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -383,7 +383,7 @@ function App() {
                 }
               />
               <Route
-                path="/UAC/Users/UserResourceMappingList"
+                path="/UAC/UserResource/View"
                 element={
                   <ProtectedRoute>
                     <UserResourceMappingList />
