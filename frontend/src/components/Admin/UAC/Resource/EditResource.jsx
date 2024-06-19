cfd2845889c268af8f96dfcb2ef4ce7d499b4244
Suraj Biswal <suraj.biswal@soulunileaders.com>
2024-06-19 11:45:19 +0530

    Create, Edit ,List resources

--- frontend/src/components/Admin/UAC/Resource/EditResource.jsx
+++ frontend/src/components/Admin/UAC/Resource/EditResource.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import "../Resource/EditResource.css";
 // Axios for (Fetch Api)--------------------------
-import axiosHttpClient from '../../../../utils/axios'
+import axiosHttpClient from "../../../../utils/axios";
 // Import Navigate and Crypto -----------------------------------
-import { decryptData } from '../../../../utils/encryptData';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { decryptData } from "../../../../utils/encryptData";
+import { useLocation, useNavigate } from "react-router-dom";
 // Import toast ---------------------------------------
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 const EditDisplayResource = () => {
-
-// useSate for get the data -------------------------------
-const[DisplayResource, setDisplayResource]=useState([])
-// useSate for Edit or post the data ------------------------
-const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+  // useSate for get the data -------------------------------
+  const [DisplayResource, setDisplayResource] = useState([]);
+  // useSate for Edit or post the data ------------------------
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
   const [description, setDescription] = useState("");
   const [parentResource, setParentResource] = useState("");
   const [path, setPath] = useState("");
@@ -23,173 +23,207 @@ const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
   //here Location / crypto and navigate the page---------------
   const location = useLocation();
-  const resourceId = decryptData(new URLSearchParams(location.search).get('resourceId'));
-  const action = new URLSearchParams(location.search).get('action');
-    const navigate = useNavigate();
+  const resourceId = decryptData(
+    new URLSearchParams(location.search).get("resourceId")
+  );
+  const action = new URLSearchParams(location.search).get("action");
+  const navigate = useNavigate();
 
-// here Get the data --------------------------------
-  async function GetDisplayResource(){
-    try{
-      let res= await axiosHttpClient('RESOURCE_VIEW_BY_ID_API', 'get', null, resourceId)
-      console.log("Get data of Edit Display resource", res)
-  
-    }
-    catch(err){
-      console.log("here Error", err)
+  // here Get the data --------------------------------
+  async function GetDisplayResource() {
+    try {
+      let res = await axiosHttpClient(
+        "RESOURCE_VIEW_BY_ID_API",
+        "get",
+        null,
+        resourceId
+      );
+      console.log("Get data of Edit Display resource", res);
+    } catch (err) {
+      console.log("here Error", err);
     }
   }
 
-  // Here Post the data --------------------------------Z 
-// Handle Submit (Onclcik of from )--------------------------------------------------
-const HandleSubmit = async (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const parentResourceValue = formData.get('parent');
-      if (isCheckboxChecked) {
-        if (!name || !description || !orderIn || !path) {
-          setErrorMessage("All fields are required!!");
-          return;
-        } else {
-          await insertResourceData(name, description, "", path, orderIn);
-        }
+  // Here Post the data --------------------------------Z
+  // Handle Submit (Onclcik of from )--------------------------------------------------
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const parentResourceValue = formData.get("parent");
+    if (isCheckboxChecked) {
+      if (!name || !description || !orderIn || !path) {
+        setErrorMessage("All fields are required!!");
+        return;
       } else {
-        if (!name || !description || !orderIn || !path || !parentResourceValue) {
-          setErrorMessage("All fields are required!!");
-          return;
-        } else {
-          await insertResourceData(name, description, parentResourceValue, path, orderIn);
-        }
+        await insertResourceData(name, description, "", path, orderIn);
       }
-}
-// Here POST the data () ----------------------------------------------------------------------------
-      const insertResourceData = async (name, description, parentResource, path, orderIn) => {
-            try {
-              let res = await axiosHttpClient('RESOURCE_CREATE_API', 'post', {
-                name: name,
-                description: description,
-                orderIn: orderIn,
-                path: path,
-                parentResourceId: parentResource || null,
-              })
-              console.log("Response:", res);
-              toast.success("Role created successfully");
-            } catch (err) {
-              console.error("Error:", err);
-            // ----------------------Toast -----------------------------
-              toast.error("Failed to create role. Please try again.");
-              setErrorMessage(err.message);
-            }
+    } else {
+      if (!name || !description || !orderIn || !path || !parentResourceValue) {
+        setErrorMessage("All fields are required!!");
+        return;
+      } else {
+        await insertResourceData(
+          name,
+          description,
+          parentResourceValue,
+          path,
+          orderIn
+        );
       }
-// Handle (Check - Checkbox)--------------------------------------------------------------------
-      const handleCheckboxChange = () => {
-            setIsCheckboxChecked(!isCheckboxChecked);
-            if (isCheckboxChecked) {
-              setParentResource("");
-            }
-      }
-// here get resoruce data ----------------------------------------------------------
-async function getResourceDataDropdown() {
-        try {
-                const res = await axiosHttpClient('RESOURCE_NAME_DROPDOWN', 'get')
-                console.log("here is Response means get data", res)
-                setParentResourceList(res.data.data);
-
-              } catch (err) {
-              console.error("Error fetching resource data:", err);
-          }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+  };
+  // Here POST the data () ----------------------------------------------------------------------------
+  const insertResourceData = async (
+    name,
+    description,
+    parentResource,
+    path,
+    orderIn
+  ) => {
+    try {
+      let res = await axiosHttpClient("RESOURCE_CREATE_API", "post", {
+        name: name,
+        description: description,
+        orderIn: orderIn,
+        path: path,
+        parentResourceId: parentResource || null,
+      });
+      console.log("Response:", res);
+      toast.success("Role created successfully");
+    } catch (err) {
+      console.error("Error:", err);
+      // ----------------------Toast -----------------------------
+      toast.error("Failed to create role. Please try again.");
+      setErrorMessage(err.message);
+    }
+  };
+  // Handle (Check - Checkbox)--------------------------------------------------------------------
+  const handleCheckboxChange = () => {
+    setIsCheckboxChecked(!isCheckboxChecked);
+    if (isCheckboxChecked) {
+      setParentResource("");
+    }
+  };
+  // here get resoruce data ----------------------------------------------------------
+  async function getResourceDataDropdown() {
+    try {
+      const res = await axiosHttpClient("RESOURCE_NAME_DROPDOWN", "get");
+      console.log("here is Response means get data", res);
+      setParentResourceList(res.data.data);
+    } catch (err) {
+      console.error("Error fetching resource data:", err);
+    }
+  }
 
   /// UseEffect for Update the data ----------------------------
-    useEffect(()=>{
-      GetDisplayResource()
-      getResourceDataDropdown() 
-    }, [])
-
+  useEffect(() => {
+    GetDisplayResource();
+    getResourceDataDropdown();
+  }, []);
 
   return (
-    <div className='container-1'>
-      {/* HEADER CREATION FOR RESOURCE LIST */}
-      <div className="header-role">
-        <div className="rectangle"></div>
-        <div className="roles">
-          <h1><b>Update Resource</b></h1>
-        </div>
-      </div>
-      {/* Back button  */}
-      <div className="back-btn">
-        <button className="btn" onClick={() => navigate('/UAC/Resources/ListOfResources')}>Back</button>
-      </div>
-      {/* Form having inputs field */}
-      <form >
-      <div className="form">
-        <div className="fields">
-          <div className="title"><h3>Resource Name</h3></div>
-          <input type="text" name="name" placeholder='Update Name' id="" className="input-fields" />
-        </div>
-
-        <div className="fields">
-          <div className="title"><h3>Resource Description</h3></div>
-          <input type="text" name="name" placeholder=' Update Resource Description' id="" className="input-fields"/>
-        </div>
-
-        <div className="fields">
-          <div className="checkbox">
-          <span>Is Parent-Resource</span> <input type="checkbox" name="Is parent-resource" id=""  />  
+    <div className="editResParent">
+      <div className="container-1">
+        {/* HEADER CREATION FOR RESOURCE LIST */}
+        <div className="header-role">
+          <div className="rectangle"></div>
+          <div className="roles">
+            <h1>
+              <b>Update Resource</b>
+            </h1>
           </div>
-        </div> 
-
-        <div className="fields">
-          <div className="title"><h3>Parent Resource Name</h3></div>
-          <select name="cars" id="cars" className="input-fields">
-            <option value="Dashboard">Dashboard</option>
-            <option value="mainforce">mainforce</option>
-            <option value="mercedes">mercedes</option>
-            <option value="durex">durex</option>
-          </select>
         </div>
-
-        <div className="fields">
-          <div className="title"><h3>Path</h3></div>
-          <input type="text" name="name" placeholder='Enter Path Ex- (/path12)' id="" className="input-fields" />
+        {/* Back button  */}
+        <div className="back-btn">
+          <button
+            className="btn"
+            onClick={() => navigate("/UAC/Resources/ListOfResources")}
+          >
+            Back
+          </button>
         </div>
+        {/* Form having inputs field */}
+        <form className="EditResForm" onSubmit={HandleSubmit}>
+          <div className="form">
+            <div className="fields">
+              <div className="title">
+                <h3>Resource Name</h3>
+              </div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Update Name"
+                id=""
+                className="input-fields"
+              />
+            </div>
 
-        <div className="fields">
-          <div className="title"><h3>Show in Order</h3></div>
-          <input type="number" name="number" id="" defaultValue={0} className="input-fields"/>
-        </div>
+            <div className="fields">
+              <div className="title">
+                <h3>Resource Description</h3>
+              </div>
+              <input
+                type="text"
+                name="name"
+                placeholder=" Update Resource Description"
+                id=""
+                className="input-fields"
+              />
+            </div>
+
+            <div className="fields">
+              <div className="checkbox">
+                <span>Is Parent-Resource</span>{" "}
+                <input type="checkbox" name="Is parent-resource" id="" />
+              </div>
+            </div>
+
+            <div className="fields">
+              <div className="title">
+                <h3>Parent Resource Name</h3>
+              </div>
+              <select name="cars" id="cars" className="input-fields">
+                <option value="Dashboard">a</option>
+                <option value="mainforce">b</option>
+                <option value="mercedes">c</option>
+                <option value="durex">d</option>
+              </select>
+            </div>
+
+            <div className="fields">
+              <div className="title">
+                <h3>Path</h3>
+              </div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Path Ex- (/path12)"
+                id=""
+                className="input-fields"
+              />
+            </div>
+
+            <div className="fields">
+              <div className="title">
+                <h3>Show in Order</h3>
+              </div>
+              <input
+                type="number"
+                name="number"
+                id=""
+                defaultValue={0}
+                className="input-fields"
+              />
+            </div>
+          </div>
+
+          <div className="buttons">
+            <div className="submit-btn">Submit</div>
+          </div>
+        </form>
       </div>
-
-      <div className="buttons">
-        <div className="submit-btn">
-          Submit
-        </div>
-      </div>
-      </form>
-
     </div>
-  )
-}
+  );
+};
 
-export default EditDisplayResource
+export default EditDisplayResource;
--- frontend/src/components/Admin/UAC/Resource/ListOfResources.jsx
+++ frontend/src/components/Admin/UAC/Resource/ListOfResources.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { Link, useNavigate } from 'react-router-dom';
-import './ListOfResources.css';
-import { encryptData } from '../../../../utils/encryptData';
+import React from "react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import "./ListOfResources.css";
+import { encryptData } from "../../../../utils/encryptData";
 
 // here import axios for fetch the data from api
-import axiosHttpClient from '../../../../utils/axios';
+import axiosHttpClient from "../../../../utils/axios";
 
 export default function ListOfResources() {
-
   let navigate = useNavigate();
-const [ListOfResources, setListofResources] =useState([])
+  const [ListOfResources, setListofResources] = useState([]);
   // here call api ( Get the data from api) -----------------------------------
-      async function GetListOfResources(){
-        try{
-          let res= await axiosHttpClient('RESOURCE_VIEWLIST_API', 'post')
-          console.log("here Response get data ",res);
-          setListofResources(res.data.Resource)
-        }
-        catch(err){
-          console.log("here Error get data ", err)
-        }
-      }
+  async function GetListOfResources() {
+    try {
+      let res = await axiosHttpClient("RESOURCE_VIEWLIST_API", "post");
+      console.log("here Response get data ", res);
+      setListofResources(res.data.Resource);
+    } catch (err) {
+      console.log("here Error get data ", err);
+    }
+  }
 
-// use useEffect for Update data ----------------------------------------------
+  // use useEffect for Update data ----------------------------------------------
 
-     useEffect(()=>{
-          GetListOfResources()
-     }, [])
+  useEffect(() => {
+    GetListOfResources();
+  }, []);
 
   // encryptid--------------------------------------------------------------
   function encryptDataId(id) {
@@ -37,101 +35,111 @@ const [ListOfResources, setListofResources] =useState([])
     return res;
   }
 
+  return (
+    <div className="CreateResParent">
+      <div className="container">
+        {/* HEADER CREATION FOR RESOURCE LIST */}
+        <div className="header-role">
+          <div className="rectangle"></div>
+          <div className="roles">
+            <h1>
+              <b> Resource List</b>
+            </h1>
+          </div>
+        </div>
+        {/* SEARCH CONTAINER AND Create new Resource BUTTON */}
+        <div className="container-size">
+          <div className="search_text_conatiner">
+            <button
+              className="create-role-btn"
+              onClick={() => navigate("/UAC/Resources/CreateResource")}
+            >
+              Create new Resource
+            </button>
+            <input
+              type="text"
+              className="search_input_field"
+              placeholder="Search..."
+              id="myInput"
+              // value={searchTerm}
+              // onChange={handleSearchChange}
+            />
+          </div>
 
-
-
-
-
-
-
-
-
-
-    return (
-        <div className='container'>
-
-            {/* HEADER CREATION FOR RESOURCE LIST */}
-            <div className="header-role">
-                <div className="rectangle"></div>
-                <div className="roles">
-                    <h1><b> Resource List</b></h1>
-                </div>
-            </div>
-            {/* SEARCH CONTAINER AND Create new Resource BUTTON */}
-            <div className="container-size">
-            <div className="search_text_conatiner">
-                <button className='create-role-btn' onClick={() => navigate('/UAC/Resources/CreateResource')}>Create new Resource</button>
-                <input
-                    type="text"
-                    className="search_input_field"
-                    placeholder="Search..."
-                    id="myInput"
-                    // value={searchTerm}
-                    // onChange={handleSearchChange}
-                />
-            </div>
-
-
-        {/* TABLE CREATION */}
-        <div className="table_Container">
-        <table >
-          <thead>
-            <tr>
-              <th scope="col">Resource Name</th>
-              <th scope="col">Parent Resource Name</th>
-              <th scope="col">Path</th>
-              <th scope="col">order</th>
-              <th scope="col">Status</th>
-              <th scope="col">View</th>
-              <th scope="col">Update</th>
-            </tr>
-          </thead>
-          <tbody>
-              {ListOfResources?.length > 0 && ListOfResources.map((item, index) => {
-                return (
-                  <tr key={index}>
-                      <td>{item.name}</td>
-                      <td>{item.parentResourceName }</td>
-                      <td>{item.path }</td>
-                      <td>{item.order}</td>
-                      <td className={`text-left ${item.status === 'ACTIVE'? 'text-green-500' : 'text-red-500'}`}>{item.status}</td>
-                      <td>
-                        <Link to={{ 
-                           pathname: '/UAC/Resources/EditDisplayResource',
-                             search: `?resourceId=${encryptDataId(item.resourceId)}&action=view`
-                              }}
-                             >
-                            <FontAwesomeIcon icon={faEye} />
-                            </Link>
+          {/* TABLE CREATION */}
+          <div className="table_Container">
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Resource Name</th>
+                  <th scope="col">Parent Resource Name</th>
+                  <th scope="col">Path</th>
+                  <th scope="col">order</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">View</th>
+                  <th scope="col">Update</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ListOfResources?.length > 0 &&
+                  ListOfResources.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{item.name}</td>
+                        <td>{item.parentResourceName}</td>
+                        <td>{item.path}</td>
+                        <td>{item.order}</td>
+                        <td
+                          className={`text-left ${
+                            item.status === "ACTIVE"
+                              ? "text-green-500"
+                              : "text-red-500"
+                          }`}
+                        >
+                          {item.status}
                         </td>
                         <td>
-                        <Link
+                          <Link
                             to={{
-                              pathname: '/UAC/Resources/EditDisplayResource',
-                             search: `?resourceId=${encryptDataId(item.resourceId)}&action=edit`
-                                 }}
-                                   >
-                                   <FontAwesomeIcon icon={faPenToSquare} />
-                                   </Link>
+                              pathname: "/UAC/Resources/EditDisplayResource",
+                              search: `?resourceId=${encryptDataId(
+                                item.resourceId
+                              )}&action=view`,
+                            }}
+                          >
+                            <FontAwesomeIcon icon={faEye} />
+                          </Link>
                         </td>
-                  </tr>
-                )
-              })}
-            </tbody>
+                        <td>
+                          <Link
+                            to={{
+                              pathname: "/UAC/Resources/EditDisplayResource",
+                              search: `?resourceId=${encryptDataId(
+                                item.resourceId
+                              )}&action=edit`,
+                            }}
+                          >
+                            <FontAwesomeIcon icon={faPenToSquare} />
+                          </Link>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
 
-        </table>
+          {/* Pagination */}
+          <div className="pagination">
+            <button onClick={() => handlePageChange("start")}>Start</button>
+            <button onClick={() => handlePageChange("previous")}>
+              Previous
+            </button>
+            <button onClick={() => handlePageChange("next")}>Next</button>
+            <button onClick={() => handlePageChange("last")}>Last</button>
+          </div>
+        </div>
       </div>
-
-      {/* Pagination */}
-      <div className="pagination">
-        <button onClick={() => handlePageChange('start')}>Start</button>
-        <button onClick={() => handlePageChange('previous')}>Previous</button>
-        <button onClick={() => handlePageChange('next')}>Next</button>
-        <button onClick={() => handlePageChange('last')}>Last</button>
-      </div>
-
-      </div>
-            </div>
-        
-    )
+    </div>
+  );
 }
