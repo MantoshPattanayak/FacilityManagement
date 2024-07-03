b5131cf19748b6813e67310b415f08615064877f
debeshdeepta <debesh.deepta@soulunileaders.com>
2024-07-03 13:46:39 +0530

    Search in Landing page
    Search in Landing page

--- frontend/src/components/Public/Search_card.jsx
+++ frontend/src/components/Public/Search_card.jsx
@@ -4,107 +4,130 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Search_card.css';
 import PublicHeader from "../../common/PublicHeader.jsx";
-
+import image from '../../assets/Anan_vihar.jpg';
 
 function Search_Card() {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToShow: 3, 
+    centerMode: true,
+    centerPadding: '10%', 
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: '15%', 
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '25%', 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '25%', 
+        }
+      }
+    ]
   };
 
   return (
     <div>
-      <PublicHeader/>
-    <div className='w'>
-      <div className="heading-text">
-        <p>Parks</p>
-      </div>
-      <div className="mt-20">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div key={d.name} className="custom-card">
-              <div className='custom-header'>
-                <img src={d.img} alt="" className="custom-image"/>
+      <PublicHeader />
+      {/* Parks................................................. */}
+      <div className='w'>
+        <div className="heading-text">
+          <p>Parks</p>
+        </div>
+        <div className="mt-20">
+          <Slider {...settings}>
+            {data.map((d) => (
+              <div className="park-card" key={d.name}>
+                <img className="Card_img" src={image} alt="Park" />
+                <div className="card_text">
+                  <span className="Name_location">
+                    <h2 className="park_name">Ananda Bana</h2>
+                    <h3 className="park_location">Baramunda, Bhubaneswar</h3>
+                  </span>
+                  <span className="Avil_Dis">
+                    <button className="Available">Available</button>
+                    <h3 className="distance">10 km(s)</h3>
+                  </span>
+                </div>
               </div>
-              <div className="custom-content">
-                <p className="custom-name">{d.name}</p>
-                <p className="custom-review">{d.review}</p>
-              </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div> 
 
-     {/* playgrounds.............................. */}
-    <div className='w'>
-      <div className="heading-text">
-        <p>Playgrounds</p>
-      </div>
-      <div className="mt-20">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div key={d.name} className="custom-card">
-              <div className='custom-header'>
-                <img src={d.img} alt="" className="custom-image"/>
+      {/* Play grounds.................................................  */}
+      <div className='w'>
+        <div className="heading-text">
+          <p>Play grounds</p>
+        </div>
+        <div className="mt-20">
+          <Slider {...settings}>
+            {data.map((d) => (
+              <div className="park-card" key={d.name}>
+                <img className="Card_img" src={image} alt="Park" />
+                <div className="card_text">
+                  <span className="Name_location">
+                    <h2 className="park_name">Ananda Bana</h2>
+                    <h3 className="park_location">Baramunda, Bhubaneswar</h3>
+                  </span>
+                  <span className="Avil_Dis">
+                    <button className="Available">Available</button>
+                    <h3 className="distance">10 km(s)</h3>
+                  </span>
+                </div>
               </div>
-              <div className="custom-content">
-                <p className="custom-name">{d.name}</p>
-                <p className="custom-review">{d.review}</p>
-              </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
 
-    {/* Multi Purpose Playgrounds....................... */}
-    <div className='w'>
-      <div className="heading-text">
-        <p>Multi Purpose Playgrounds</p>
-      </div>
-      <div className="mt-20">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div key={d.name} className="custom-card">
-              <div className='custom-header'>
-                <img src={d.img} alt="" className="custom-image"/>
+      {/* For Events........................................................ */}
+      <div className='w'>
+        <div className="heading-text">
+          <p>Events</p>
+        </div>
+        <div className="mt-20">
+          <Slider {...settings}>
+            {data.map((d) => (
+              <div className="park-card" key={d.name}>
+                <img className="Card_img" src={image} alt="Park" />
+                <div className="card_text">
+                  <span className="Name_location">
+                    <h2 className="park_name">Ananda Bana</h2>
+                    <h3 className="park_location">Baramunda, Bhubaneswar</h3>
+                  </span>
+                  <span className="Avil_Dis">
+                    <button className="Available">Available</button>
+                    <h3 className="distance">10 km(s)</h3>
+                  </span>
+                </div>
               </div>
-              <div className="custom-content">
-                <p className="custom-name">{d.name}</p>
-                <p className="custom-review">{d.review}</p>
-              </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
-
-{/* Events.................... */}
-<div className='w'>
-      <div className="heading-text">
-        <p>Events</p>
-      </div>
-      <div className="mt-20">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div key={d.name} className="custom-card">
-              <div className='custom-header'>
-                <img src={d.img} alt="" className="custom-image"/>
-              </div>
-              <div className="custom-content">
-                <p className="custom-name">{d.name}</p>
-                <p className="custom-review">{d.review}</p>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
-    </div>
-
     </div>
   );
 }
@@ -121,7 +144,7 @@ const data = [
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
   },
   {
-    name: `Akay sharma`,
+    name: `Akay Sharma`,
     img: `/students/Nia_Adebayo.jpg`,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
   },
@@ -131,7 +154,7 @@ const data = [
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
   },
   {
-    name: `Matru prashad`,
+    name: `Matru Prashad`,
     img: `/students/Mia_Williams.jpg`,
     review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
   },
