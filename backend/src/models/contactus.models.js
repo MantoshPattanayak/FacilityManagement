e8df34b726b17e353502f1e51b9d361d54ab968a
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-07-03 11:15:54 +0530

    added the contact us model and remove about and facilitytype column from facility table

---
+++ backend/src/models/contactus.models.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize,DataTypes)=>{
+    let contactus = sequelize.define('contactrequest',{
+       contactId:{
+           type:DataTypes.INTEGER,
+           primaryKey:true,
+           autoIncrement:true
+       },
+       fullName:{
+           type:DataTypes.STRING
+       },
+       emailId:{
+           type:DataTypes.STRING
+       },
+       mobileNo:{
+           type:DataTypes.BIGINT
+       },
+       message:{
+           type:DataTypes.TEXT
+       },
+       statusId:{
+            type:DataTypes.INTEGER
+       },
+       createdDt:{
+           type:DataTypes.DATE
+       },
+       updatedDt:{
+           type:DataTypes.DATE
+       }
+   
+    },
+    {
+       timestamps:false
+   })
+    return contactus
+   }
