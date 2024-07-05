5abf98cda1d4c3803ebc10460cda5c11ef22a94f
MantoshPattanayak1 <126453031+MantoshPattanayak1@users.noreply.github.com>
2024-07-05 13:38:49 +0530

    added the facility name "

--- backend/src/controllers/v1/mdm/tariff.controllers.js
+++ backend/src/controllers/v1/mdm/tariff.controllers.js
@@ -196,7 +196,7 @@ let updateTariff = async (req,res)=>{
     try {
         let {facilityTariffData} = req.body
         let statusId = 1;
-        if(facilityTariffData.length>0 || facilityTariffData.some((tariffData)=>{!tariffData.facilityId || !tariffData.facilityId || !tariffData.operatingHoursFrom || !tariffData.operatingHoursTo  || !tariffData.dayWeek || !tariffData.validityFrom  || !tariffData.validityTo || !tariffData.statusId || !tariffData.tariffMasterId || !tariffData.tariffDetailId})){
+        if(facilityTariffData.length>0 || facilityTariffData.some((tariffData)=>{!tariffData.facilityId || !tariffData.facilityId || !tariffData.operatingHoursFrom || !tariffData.operatingHoursTo  || !tariffData.dayWeek || !tariffData.validityFrom  || !tariffData.validityTo || !tariffData.statusId || !tariffData.tariffMasterId || !tariffData.tariffDetailId || !tariffData.tariffTypeId || !tariffData.entityId})){
             return res.status(statusCode.BAD_REQUEST.code).json({
                 message:"Please provide all required fields"
             })
@@ -535,6 +535,7 @@ let initialDataForTariffSelectionWRTCategory = async (req,res)=>{
     }
         return res.status(statusCode.SUCCESS.code).json({
             message:"Initial Data for tariff",
+            facilityData:findFacilityTypeIdFromFacilityTable,
             tariffTypeData:tariffTypeQuery,
             activityData:findTheNameOfThoseActivities,
             sportsData:findTheNameOfThoseSports,
