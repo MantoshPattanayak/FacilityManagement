3b0afdfd1e6230f8e56603cb965e1c18ac13ea94
Alok Kumar <alok.kumar@soulunileaders.com>
2024-08-05 16:37:31 +0530

    Resolve the bugs
    Resolve the bugs

---
+++ frontend/src/common/ShimmerUIFlex.jsx
@@ -0,0 +1,48 @@
+import './ShimmerUIFlex.css'
+const ShimmerUIFlex=()=>{
+    return(
+        <div>
+        <div className="shimmer">
+        <div className="wrapper">
+          <div className="image-card animate"></div>
+          <div className="stroke animate title"></div>
+          <div class="stroke animate link"></div>
+          <div class="stroke animate description"></div>
+        </div>
+      </div>
+      <div className="shimmer">
+        <div className="wrapper">
+          <div className="image-card animate"></div>
+          <div className="stroke animate title"></div>
+          <div class="stroke animate link"></div>
+          <div class="stroke animate description"></div>
+        </div>
+      </div>
+      <div className="shimmer">
+        <div className="wrapper">
+          <div className="image-card animate"></div>
+          <div className="stroke animate title"></div>
+          <div class="stroke animate link"></div>
+          <div class="stroke animate description"></div>
+        </div>
+      </div>
+      <div className="shimmer">
+        <div className="wrapper">
+          <div className="image-card animate"></div>
+          <div className="stroke animate title"></div>
+          <div class="stroke animate link"></div>
+          <div class="stroke animate description"></div>
+        </div>
+      </div>
+      <div className="shimmer">
+        <div className="wrapper">
+          <div className="image-card animate"></div>
+          <div className="stroke animate title"></div>
+          <div class="stroke animate link"></div>
+          <div class="stroke animate description"></div>
+        </div>
+      </div>
+      </div>
+    )
+}
+export default ShimmerUIFlex;
--- frontend/src/components/Public/Search_Card/ShimmerUI.jsx
+++ frontend/src/components/Public/Search_Card/ShimmerUI.jsx
@@ -1,106 +1,105 @@
 
 // Create Funcation of ShimmerUI---------------------------------------------------------------------------
 import "./ShimmerUI.css"
-const ShimmerUI=()=>{
+const ShimmerUI = () => {
 
-    // here Retunr function ---------------------------------------------------------------------------------
-    return(
-        <div>
-   <div className="main_card">
-          <div className="card">
+  // here Retunr function ---------------------------------------------------------------------------------
+  return (
+    <div>
+      <div className="main_card">
+        <div className="card">
           <div className="shimmerBG media"></div>
           <div className="p-32">
-           <div className="shimmerBG title-line"></div>
-           <div className="shimmerBG title-line end"></div>
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
 
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
-        </div>
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
         <div className="card">
-        <div className="shimmerBG media"></div>
-        <div className="p-32">
-          <div className="shimmerBG title-line"></div>
-          <div className="shimmerBG title-line end"></div>
-
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
-        </div>
-        </div>
-        <div className="card">
-        <div className="shimmerBG media"></div>
-        <div className="p-32">
-          <div className="shimmerBG title-line"></div>
-          <div className="shimmerBG title-line end"></div>
-
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
-        </div>
-        </div>
-     
-     
-    
-        
-        
-        </div>
-        <div className="main_card">
-          <div className="card">
           <div className="shimmerBG media"></div>
           <div className="p-32">
-           <div className="shimmerBG title-line"></div>
-           <div className="shimmerBG title-line end"></div>
-
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
-        </div>
-        </div>
-        <div className="card">
-        <div className="shimmerBG media"></div>
-        <div className="p-32">
-          <div className="shimmerBG title-line"></div>
-          <div className="shimmerBG title-line end"></div>
-
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
-        </div>
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
         <div className="card">
-        <div className="shimmerBG media"></div>
-        <div className="p-32">
-          <div className="shimmerBG title-line"></div>
-          <div className="shimmerBG title-line end"></div>
+          <div className="shimmerBG media"></div>
+          <div className="p-32">
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
 
-          <div className="shimmerBG content-line m-t-24"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line"></div>
-          <div className="shimmerBG content-line end"></div>
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
+
+
+
+
+
+      </div>
+      <div className="main_card">
+        <div className="card">
+          <div className="shimmerBG media"></div>
+          <div className="p-32">
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
+
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
-     
-     
-    
-        
-        
+        <div className="card">
+          <div className="shimmerBG media"></div>
+          <div className="p-32">
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
+
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
+        <div className="card">
+          <div className="shimmerBG media"></div>
+          <div className="p-32">
+            <div className="shimmerBG title-line"></div>
+            <div className="shimmerBG title-line end"></div>
+
+            <div className="shimmerBG content-line m-t-24"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line"></div>
+            <div className="shimmerBG content-line end"></div>
+          </div>
         </div>
-     
-    )
+
+
+
+
+
+      </div>
+    </div>
+
+  )
 }
 
 // Export shimmer UI
