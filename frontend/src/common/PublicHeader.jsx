3f62111d48a48b9bc68440e693a3b716dce8ba3e
surajbiswal2000 <163093431+surajbiswal2000@users.noreply.github.com>
2024-08-08 11:50:22 +0530

    Main_Body_Park

--- frontend/src/common/PublicHeader.jsx
+++ frontend/src/common/PublicHeader.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import AppLogo from "../assets/ama-bhoomi_logo.png";
 // import "../components/Public/Landing";
-import '../common/PublicHeader.css';
+import "../common/PublicHeader.css";
 // Font Awesome icon --------------------------------
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPowerOff, faUser, faBars, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPowerOff,
+  faUser,
+  faBars,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
 import axiosHttpClient from "../utils/axios";
 // Import Redux Part ---------------------------------
-import { useDispatch, useSelector } from "react-redux";  // selector use for Read the data -----------------
+import { useDispatch, useSelector } from "react-redux"; // selector use for Read the data -----------------
 import { setLanguage, setLanguageContent } from "../utils/languageSlice";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Logout } from "../utils/authSlice";
@@ -17,18 +22,25 @@ import { toast } from "react-toastify";
 export default function PublicHeader() {
   const [showMediaIcon, setShowMediaIcon] = useState(false);
   const [GetCardCount, setGetCardCount] = useState([]);
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(useSelector((state => state.auth.isUserLoggedIn)));
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(
+    useSelector((state) => state.auth.isUserLoggedIn)
+  );
   const [refreshOnLogOut, setRefreshOnLogOut] = useState(false);
   // show the Popup (sub_manu) Click on Profile ---------------------
   const [showProfileMenu, setShowProfileMenu] = useState(false);
 
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const language = useSelector((state) => state.language.language || localStorage.getItem("language"));
-  const languageContent = useSelector((state) => state.language.languageContent) || JSON.parse(localStorage.getItem("languageContent"));
-  const isLanguageContentFetched = useSelector((state) => state.language.isLanguageContentFetched);
+  const language = useSelector(
+    (state) => state.language.language || localStorage.getItem("language")
+  );
+  const languageContent =
+    useSelector((state) => state.language.languageContent) ||
+    JSON.parse(localStorage.getItem("languageContent"));
+  const isLanguageContentFetched = useSelector(
+    (state) => state.language.isLanguageContentFetched
+  );
   const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
@@ -41,14 +53,20 @@ export default function PublicHeader() {
   // get content means odia and eng ----------------------
   async function getWebContent() {
     try {
-      const res = await axiosHttpClient('LANGUAGE_RESOURCE_API', 'post', { language });
+      const res = await axiosHttpClient("LANGUAGE_RESOURCE_API", "post", {
+        language,
+      });
       // if (Array.isArray(res.data.languageContentResultData)) {
       dispatch(setLanguageContent(res.data.languageContentResultData));
       // } else {
-      console.log('valid language content data:', language, res.data.languageContentResultData);
+      console.log(
+        "valid language content data:",
+        language,
+        res.data.languageContentResultData
+      );
       // }
     } catch (error) {
-      console.error('Error fetching language content:', error);
+      console.error("Error fetching language content:", error);
     }
   }
 
@@ -58,9 +76,9 @@ export default function PublicHeader() {
   /// get total count --------------------
   async function GetTotalNumberofCart() {
     try {
-      let res = await axiosHttpClient('View_Card_UserId', 'get');
+      let res = await axiosHttpClient("View_Card_UserId", "get");
       setGetCardCount(res.data);
-      console.log("total number of cart", res)
+      console.log("total number of cart", res);
     } catch (err) {
       console.log(err);
     }
@@ -68,13 +86,11 @@ export default function PublicHeader() {
 
   useEffect(() => {
     getWebContent();
-    if (isUserLoggedIn)
-      GetTotalNumberofCart();
+    if (isUserLoggedIn) GetTotalNumberofCart();
   }, []);
 
   useEffect(() => {
-    if (refreshOnLogOut)
-      setIsUserLoggedIn(0);
+    if (refreshOnLogOut) setIsUserLoggedIn(0);
   }, [refreshOnLogOut]);
 
   // here Api of Logout -------------------------
@@ -82,21 +98,20 @@ export default function PublicHeader() {
     // logOutUser(e);
     async function logOutAPI() {
       try {
-        let res = await axiosHttpClient('LOGOUT_API', 'post');
+        let res = await axiosHttpClient("LOGOUT_API", "post");
         console.log(res.data);
         dispatch(Logout());
         setRefreshOnLogOut(!refreshOnLogOut);
-        toast.success('Logged out successfully!!', {
+        toast.success("Logged out successfully!!", {
           autoClose: 2000, // Toast timer duration in milliseconds
           onClose: () => {
             // Navigate to another page after toast timer completes
             setTimeout(() => {
-              navigate('/')
+              navigate("/");
             }, 1000); // Wait 1 second after toast timer completes before navigating
           },
         });
-      }
-      catch (error) {
+      } catch (error) {
         console.error(error);
       }
     }
@@ -104,7 +119,7 @@ export default function PublicHeader() {
   }
 
   function handleMenuToggle() {
-    setShowMediaIcon(prevState => !prevState);
+    setShowMediaIcon((prevState) => !prevState);
   }
   // here Toggle when Click on profile icon ----------------------------
   const handleMouseEnter = () => {
@@ -121,50 +136,108 @@ export default function PublicHeader() {
         <div className="header-content">
           <div className="logo-ama-boomi">
             <Link to={"/"}>
-              <img src={AppLogo} alt="App Logo" className="h-[100%] top-0 absolute" />
+              <img
+                src={AppLogo}
+                alt="App Logo"
+                className="h-[100%] top-0 absolute"
+              />
             </Link>
           </div>
           <div className="navbar">
-            <ul className={showMediaIcon ? "hidden menu_links mobile_menu_links show" : "hidden menu_links mobile_menu_links"}>
+            <ul
+              className={
+                showMediaIcon
+                  ? "hidden menu_links mobile_menu_links show"
+                  : "hidden menu_links mobile_menu_links"
+              }
+            >
               <li>
-                {(language === 'EN') && <><button value={'OD'} onClick={() => { setLanguageCode('OD'); setRefresh(prevState => !prevState); }}>ଓଡ଼ିଆ</button> &nbsp; | </>}
-                {(language === 'OD') && <><button value={'EN'} onClick={() => { setLanguageCode('EN'); setRefresh(prevState => !prevState); }}>English</button> &nbsp; | </>}
+                {language === "EN" && (
+                  <>
+                    <button
+                      value={"OD"}
+                      onClick={() => {
+                        setLanguageCode("OD");
+                        setRefresh((prevState) => !prevState);
+                      }}
+                    >
+                      ଓଡ଼ିଆ
+                    </button>{" "}
+                    &nbsp; |{" "}
+                  </>
+                )}
+                {language === "OD" && (
+                  <>
+                    <button
+                      value={"EN"}
+                      onClick={() => {
+                        setLanguageCode("EN");
+                        setRefresh((prevState) => !prevState);
+                      }}
+                    >
+                      English
+                    </button>{" "}
+                    &nbsp; |{" "}
+                  </>
+                )}
               </li>
               {/* <li>
                 <Link to={'/'}>{(languageContent.find(data => data.languageResourceKey == 'publicHeaderHome')?.languageResourceValue)?.toUpperCase()}</Link>
               </li> */}
               <li>
-                <Link to={'/About'}>{(languageContent.find(data => data.languageResourceKey == 'publicHeaderAbout')?.languageResourceValue)?.toUpperCase()}</Link>
+                <Link to={"/About"}>
+                  {languageContent
+                    .find(
+                      (data) => data.languageResourceKey == "publicHeaderAbout"
+                    )
+                    ?.languageResourceValue?.toUpperCase()}
+                </Link>
               </li>
               <li>
-                <Link to={'/faqs'}>FAQ</Link>
+                <Link to={"/faqs"}>FAQ</Link>
               </li>
               <li>
-                <Link to={'/facilities'}>FACILITIES</Link>
+                <Link to={"/facilities"}>FACILITIES</Link>
               </li>
               <li>
-                <Link to={'/events'}>EVENTS</Link>
+                <Link to={"/events"}>EVENTS</Link>
               </li>
               <li>
                 <Link to={"/ContactUs"}>CONTACT US</Link>
               </li>
 
               {isUserLoggedIn == 1 ? (
-                <li className="menu-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                  <div style={{ cursor: 'pointer' }}>
+                <li
+                  className="menu-item"
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <div style={{ cursor: "pointer" }}>
                     <FontAwesomeIcon icon={faUser} /> &nbsp; PROFILE
                   </div>
                   {showProfileMenu && (
                     <ul className="submenu">
-                      <li><Link to="/profile">My Profile</Link></li>
+                      <li>
+                        <Link to="/profile">My Profile</Link>
+                      </li>
                       {isUserLoggedIn == 1 && (
                         <li>
-                          <Link to={'/Event_hostPage'}> Host Event</Link>
+                          <Link to={"/Event_hostPage"}> Host Event</Link>
                         </li>
                       )}
-                      <li><Link to="/profile/booking-details">Booking Details</Link></li>
-                      <li><Link to="/UserProfile/Favorites">Favorites</Link></li>
-                      <li><Link to={"/grievance-feedback-form"}>Submit Grievance</Link></li>
+                      <li>
+                        <Link to="/profile/booking-details">
+                          Booking Details
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/UserProfile/Favorites">Favorites</Link>
+                      </li>
+                      <li>
+                        <Link to={"/grievance-feedback-form"}>
+                          Submit Grievance
+                        </Link>
+                      </li>
                       {isUserLoggedIn == 1 && (
                         <li>
                           <Link onClick={handleLogout}>Logout</Link>
@@ -182,15 +255,26 @@ export default function PublicHeader() {
               )}
               {isUserLoggedIn == 1 && (
                 <li>
-                  <Link className="relative flex items-center" to="/cart-details">
+                  <Link
+                    className="relative flex items-center"
+                    to="/cart-details"
+                  >
                     {GetCardCount.count > 0 && (
-                      <span className="cart-count absolute left-1 transform -translate-x-1/2 -top-5 bg-red-500 text-white text-xs font-semibold py-0.5 px-1 rounded-full">{GetCardCount.count}</span>
+                      <span className="cart-count absolute left-1 transform -translate-x-1/2 -top-5 bg-red-500 text-white text-xs font-semibold py-0.5 px-1 rounded-full">
+                        {GetCardCount.count}
+                      </span>
                     )}
-                    <span><FontAwesomeIcon icon={faShoppingCart} className="cart-icon" size="lg" /> CART</span>
+                    <span>
+                      <FontAwesomeIcon
+                        icon={faShoppingCart}
+                        className="cart-icon"
+                        size="lg"
+                      />{" "}
+                      CART
+                    </span>
                   </Link>
                 </li>
               )}
-
             </ul>
             <div className="hamburger-menu" onClick={handleMenuToggle}>
               <FontAwesomeIcon icon={faBars} />
@@ -199,6 +283,5 @@ export default function PublicHeader() {
         </div>
       </header>
     </div>
-
   );
 }
