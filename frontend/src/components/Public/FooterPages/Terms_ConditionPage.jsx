56b6f3ef7e89ce8c3e5f43e16647cb7037d4e597
ramya-naik <ramya.naik@soulunileaders.com>
2024-08-13 18:07:23 +0530

    FAQs, DoDont, Terms, Privacy, Disclaimer, Common Footer

--- frontend/src/components/Public/FooterPages/Terms_ConditionPage.jsx
+++ frontend/src/components/Public/FooterPages/Terms_ConditionPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PublicHeader from '../../../common/PublicHeader';
 import './Terms_ConditionPage.css';
+import { Link } from 'react-router-dom';
 
 const Terms_ConditionPage = () => {
   return (
@@ -10,22 +11,84 @@ const Terms_ConditionPage = () => {
       
       <h1 className="terms-header">Terms and Conditions</h1>
       <div className="terms-content">
-        <h2>Introduction</h2>
-        <p>Welcome to our website. By continuing to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern [Your Company]'s relationship with you in relation to this website.</p>
+        <p>Welcome to AMA BHOOMI!</p>
+        <p>These terms and conditions outline the rules and regulations for the use of AMA BHOOMI Portal. By accessing this website, we assume you accept these terms and conditions. Do not continue to use AMA BHOOMI website/application if you do not agree to take all of the terms and conditions stated on this page.</p>
+        <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Odisha. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+        <h2>Cookies</h2>
+        <p>We employ the use of cookies. By accessing AMA BHOOMI Website/Application, you agreed to use cookies in agreement with the AMA BHOOMI Portal 's Privacy Policy.</p>
+        <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+        <h2>License</h2>
+        <p>Unless otherwise stated, AMA BHOOMI Portal and/or its licensors own the intellectual property rights for all material on AMA BHOOMI portal. All intellectual property rights are reserved. You may access this from AMA BHOOMI website/application for your own personal use subjected to restrictions set in these terms and conditions.</p>
+        <h2>You must not:</h2>
+        <p>
+          <li>Republish material from AMA BHOOMI portal</li>
+          <li>Sell, rent or sub-license material from AMA BHOOMI portal</li>
+          <li>Reproduce, duplicate or copy material from AMA BHOOMI portal</li>
+          <li>Redistribute content from AMA BHOOMI portal</li>
+          <li>This Agreement shall begin on the date hereof.</li>
+        </p>
         
-        <h2>Use of the Website</h2>
-        <p>This website may use cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by third parties.</p>
-
-        <h2>Intellectual Property</h2>
-        <p>The content on this website is for your general information and use only. It is subject to change without notice.</p>
-
-        <h2>Termination of Use</h2>
-        <p>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable.</p>
-
-        <h2>Governing Law</h2>
-        <p>Your use of this website and any dispute arising out of such use of the website is subject to the laws of [Your Country/State].</p>
-        <h2>Contact</h2>
-        <p>Please visit our Help Center with any questions regarding the Services, or submit a support ticket with any questions regarding these Terms, Service, account, or billing matters. You may also contact us by email at help@BDA.com.</p>
+        <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. AMA BHOOMI portal does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of AMA BHOOMI portal, its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, AMA BHOOMI portal shall not be liable for the comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the comments on this website.
+        AMA BHOOMI portal reserves the right to monitor all comments and to remove any comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+        <h2>You warrant and represent that:</h2>
+        <p>
+          <li>You are entitled to post the comments on our website and have all necessary licenses and consents to do so;</li>
+          <li>The comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+          <li>The comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+          <li>The comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+          <li>You hereby grant AMA BHOOMI portal a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your comments in any and all forms, formats or media.</li>
+          <li>
+            Hyperlinking to our Content: The following organizations may link to our Website without prior written approval:
+            <ul className='ml-5'>
+              <li>1. Government agencies;</li>
+              <li>2. Search engines;</li>
+              <li>3. News organizations;</li>
+            </ul>
+          </li>
+          <li>Online directory distributors may link to our website in the same manner as they hyperlink to the Websites of other listed businesses; and Systemwide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Website.</li>
+          <li>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</li>
+          <li>
+            We may consider and approve other link requests from the following types of organizations:
+            <ul className='ml-5'>
+              <li>1. commonly-known consumer and/or business information sources;</li>
+              <li>2. dot.com community sites;</li>
+              <li>3. associations or other groups representing charities;</li>
+              <li>4. online directory distributors;</li>
+              <li>5. internet portals;</li>
+              <li>6. accounting, law and consulting firms; and</li>
+              <li>7. educational institutions and trade associations.</li>
+            </ul>
+          </li>
+          <li>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavourably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of AMA BHOOMI portal; and (d) the link is in the context of general resource information.</li>
+          <li>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by contacting us on AMA BHOOMI portal. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. The response time is expected to be 2-3 weeks.</li>
+          <li>Approved organizations may hyperlink to our Portal as follows:
+            <li>By use of our corporate name; or</li>
+            <li>By use of the uniform resource locator being linked to; or</li>
+            <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
+            <li>No use of AMA BHOOMI logo or other artwork will be allowed for linking absent a trademark license agreement.</li>
+          </li>
+        </p>
+        <h2>iFrames</h2>
+        <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
+        <h2>Content Liability</h2>
+        <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+        <h2>Your Privacy</h2>
+        <p>Please read our <Link to={"/Privacy_Policy"} className='text-blue-500'>Privacy Policy</Link>.</p>
+        <h2>Reservation of Rights</h2>
+        <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amend these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+        <h2>Removal of links from our website</h2>
+        <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+        <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+        <h2>Disclaimer</h2>
+        <p>
+          To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:
+          <li>limit or exclude our or your liability for death or personal injury;</li>
+          <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+          <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+          <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+          <li>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</li>
+          <li>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</li>
+        </p>
       </div>
     </div>
   );
