1bbeec68609046a7f7624b1d480927e35ccd4ac3
Alok Kumar <alok.kumar@soulunileaders.com>
2024-08-28 17:55:30 +0530

    Employee Attendance System (Upload the excel or Csv file for Employee attendance)
    Employee Attendance System (Upload the excel or Csv file for Employee attendance)

---
+++ frontend/src/components/Admin/Emp_Attendance/Emp_Details_List.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+const Emp_Details_List=()=>{
+    return(
+        <div>
+            <h1> Table of Emp List</h1>
+        </div>
+    )
+}
+export default Emp_Details_List;
